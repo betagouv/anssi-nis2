440825e630aff07395611b3db83b2d8af9cd42ce
Bastien Gallay <bgallay@palo-it.com>
2023-08-17 14:09:01 +0200

    fix: fix icons usage

--- anssi-nis2-ui/src/App.tsx
+++ anssi-nis2-ui/src/App.tsx
@@ -4,48 +4,40 @@ import AnssiLogo from './assets/ANSSI.svg'
 import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css'
 
 function App() {
-  return (
-    <>
-      <Header
-        brandTop={<>R&Eacute;PUBLIQUE<br />FRAN&Ccedil;AISE</>}
-        homeLinkProps={{
-          href: '/',
-          title: 'Accueil - MonParcoursNIS2 - ANSSI'
-        }}
-        id="fr-header-header-with-quick-access-items"
-        operatorLogo={{
-          alt: 'Logotype ANSSI',
-          imgUrl: AnssiLogo,
-          orientation: 'horizontal'
-        }}
-        quickAccessItems={[
-          {
-            iconId: 'fr-icon-add-circle-line',
-            linkProps: {
-              href: '#'
-            },
-            text: 'Webinaire de présentation'
-          },
-          {
-            iconId: 'fr-icon-mail-fill',
-            linkProps: {
-              href: '#'
-            },
-            text: 'FAQ NIS2'
-          },
-          {
-            buttonProps: {
-              onClick: function noRefCheck() { }
-            },
-            iconId: 'ri-account-box-line',
-            text: 'Se connecter'
-          }
-        ]}
-        serviceTagline=""
-        serviceTitle="MonParcoursNIS2"
-      />
-    </>
-  )
+  return <>
+    <Header
+      brandTop={<>R&Eacute;PUBLIQUE<br />FRAN&Ccedil;AISE</>}
+      homeLinkProps={{
+        href: '/',
+        title: 'Accueil - MonParcoursNIS2 - ANSSI'
+      }}
+      id="fr-header-header-with-quick-access-items"
+      operatorLogo={{
+        alt: 'Logotype ANSSI',
+        imgUrl: AnssiLogo,
+        orientation: 'horizontal'
+      }}
+      quickAccessItems={[
+        {
+          iconId: 'fr-icon-live-fill',
+          linkProps: { href: '#' },
+          text: 'Webinaire de présentation'
+        },
+        {
+          iconId: 'fr-icon-question-fill',
+          linkProps: { href: '#' },
+          text: 'FAQ NIS2'
+        },
+        {
+          buttonProps: { onClick: () => { } },
+          iconId: "fr-icon-arrow-right-line",
+          text: 'Je m\'inscrits'
+        }
+      ]}
+      serviceTagline=""
+      serviceTitle="MonParcoursNIS2"
+    />
+  </>;
 }
 
 export default App
