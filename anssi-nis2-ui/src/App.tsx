ce43c775ba1bd2227374ca291e3e46bd4ba83773
Bastien Gallay <bgallay@palo-it.com>
2023-08-17 17:35:56 +0200

    wip: ajout du premier bloc première page

--- anssi-nis2-ui/src/App.tsx
+++ anssi-nis2-ui/src/App.tsx
@@ -1,61 +1,30 @@
 import './App.css'
-import { Header } from "@codegouvfr/react-dsfr/Header"
-import { Footer } from "@codegouvfr/react-dsfr/Footer"
 
 import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css'
 
-import AnssiLogo from './assets/ANSSI.svg'
-import HabillageAccueil from "./assets/habillage-accueil-01.svg"
+import EnTete from './Components/EnTete'
+import PiedDePage from './Components/PiedDePage'
+import { Button } from "@codegouvfr/react-dsfr/Button";
 
 function App() {
   return <>
-    <div style={{ "minHeight": "100vh", "display": "flex", "flexDirection": "column" }}>
-      <Header
-        brandTop={<>R&Eacute;PUBLIQUE<br />FRAN&Ccedil;AISE</>}
-        homeLinkProps={{
-          href: '/',
-          title: 'Accueil - MonParcoursNIS2 - ANSSI'
-        }}
-        id="fr-header-header-with-quick-access-items"
-        operatorLogo={{
-          alt: 'Logotype ANSSI',
-          imgUrl: AnssiLogo,
-          orientation: 'horizontal'
-        }}
-        quickAccessItems={[
-          {
-            iconId: 'fr-icon-live-fill',
-            linkProps: { href: '#' },
-            text: 'Webinaire de présentation'
-          },
-          {
-            iconId: 'fr-icon-question-fill',
-            linkProps: { href: '#' },
-            text: 'FAQ NIS2'
-          },
-          {
-            buttonProps: { onClick: () => { } },
-            iconId: "fr-icon-arrow-right-line",
-            text: 'Je m\'inscrits'
-          }
-        ]}
-        serviceTagline=""
-        serviceTitle="MonParcoursNIS2"
-      />
-
-      <Footer
-        contentDescription={<>
-          MonParcoursNIS2 aide les entités publiques et privées à sécuriser et homologuer leurs services numériques au prisme des obligations de la directive européenne NIS2.
-          <br />
-          <br />
-          Il est développé par l’Agence nationale de la sécurité des systèmes d’information, en lien avec BetaGouv de la Direction interministérielle du numérique.
-        </>}
-        websiteMapLinkProps={{href: '#'}}
-        accessibility="partially compliant"
-        accessibilityLinkProps={{href: '#'}}
-        termsLinkProps={{href: '#'}}
-      />
-    </div>
+    <EnTete />
+    <main className="homepage" role="main">
+      <div className="main-nis2">
+        <div className="limiter">
+          <div className="appel-a-action">
+            <h1>MonParcoursNIS2</h1>
+            <p>Accompagner les organisations dans la compréhension et la mise en conformité à NIS2</p>
+            <p>Gratuit et 100% en ligne</p>
+            <p>Au regard de la nouvelle directive NIS2, renforcez la sécurisation de vos systèmes d’information grâce à l’accompagnement et aux services de l’ANSSI.</p>
+            <Button onClick={function noRefCheck() { }}>
+              Label button
+            </Button>
+          </div>
+        </div>
+      </div>
+    </main>
+    <PiedDePage />
   </>;
 }
 
