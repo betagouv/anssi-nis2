f72a3205b518778cdef1603900846fe2d9fc8c68
Bastien Gallay <bgallay@palo-it.com>
2023-08-17 12:28:03 +0200

    fix: use correct dslfr module

--- anssi-nis2-ui/src/App.tsx
+++ anssi-nis2-ui/src/App.tsx
@@ -1,48 +1,49 @@
 import './App.css'
-import { Col, Container, Header, HeaderBody, HeaderOperator, Icon, Logo, Row, Service, Tool, ToolItem, ToolItemGroup } from '@dataesr/react-dsfr';
-import AnssiLogo from './assets/anssi.png'
-import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css';
+import { Header } from "@codegouvfr/react-dsfr/Header";
+import AnssiLogo from './assets/ANSSI.svg'
+import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css'
 
 function App() {
   return (
     <>
-      <Header>
-        <HeaderBody>
-          <Logo>République Française</Logo>
-          <HeaderOperator>
-            <img
-              alt="Logotype ANSSI"
-              src={AnssiLogo}
-            />
-          </HeaderOperator>
-          <Service
-            description=""
-            title="MonParcoursNIS2"
-          />
-
-          <Tool>
-            <ToolItemGroup>
-              <ToolItem icon="ri-live-fill" link="/">
-                Webinaire de présentation
-              </ToolItem>
-              <ToolItem icon="ri-arrow-right-line" link="/">
-                FAQ NIS2
-              </ToolItem>
-            </ToolItemGroup>
-          </Tool>
-        </HeaderBody>
-      </Header>
-      <Container>
-        <Row>
-          <Col>
-            <Icon
-              name="ri-alert-fill"
-              size="2x"
-              title="Here is a title"
-            />
-          </Col>
-        </Row>
-      </Container>
+      <Header
+        brandTop={<>R&Eacute;PUBLIQUE<br />FRAN&Ccedil;AISE</>}
+        homeLinkProps={{
+          href: '/',
+          title: 'Accueil - MonParcoursNIS2 - ANSSI'
+        }}
+        id="fr-header-header-with-quick-access-items"
+        operatorLogo={{
+          alt: 'Logotype ANSSI',
+          imgUrl: AnssiLogo,
+          orientation: 'horizontal'
+        }}
+        quickAccessItems={[
+          {
+            iconId: 'fr-icon-add-circle-line',
+            linkProps: {
+              href: '#'
+            },
+            text: 'Webinaire de présentation'
+          },
+          {
+            iconId: 'fr-icon-mail-fill',
+            linkProps: {
+              href: '#'
+            },
+            text: 'FAQ NIS2'
+          },
+          {
+            buttonProps: {
+              onClick: function noRefCheck() { }
+            },
+            iconId: 'ri-account-box-line',
+            text: 'Se connecter'
+          }
+        ]}
+        serviceTagline=""
+        serviceTitle="MonParcoursNIS2"
+      />
     </>
   )
 }
--- anssi-nis2-ui/src/main.tsx
+++ anssi-nis2-ui/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+startReactDsfr({ defaultColorScheme: "system" });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
