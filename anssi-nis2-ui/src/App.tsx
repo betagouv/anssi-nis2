2a2e48e20b96a83dcda96cfcce9feeafb5c65619
Bastien Gallay <bgallay@palo-it.com>
2023-08-21 12:22:45 +0200

    wip: simplification des classes des bandeaux

--- anssi-nis2-ui/src/App.tsx
+++ anssi-nis2-ui/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 
 import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css'
+import '@gouvfr/dsfr/dist/utility/utility.css'
 //NOTE: If you get "SyntaxError: Cannot use import statement outside a module" add tss-react here in your next.config.js: https://github.com/garronej/react-dsfr-next-demo/blob/43ecfa03d5416f2446b6867af65c7e3c7e7e41ef/next.config.js#L14
 import EnTete from './Components/EnTete'
 import PiedDePage from './Components/PiedDePage'
