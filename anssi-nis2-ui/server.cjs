79aeaaf7cd97b3068ef609f0b069b0f5a2bf3d53
Bastien Gallay <bgallay@palo-it.com>
2023-08-25 10:41:06 +0200

    Ajout de basic auth pour le lancement static de l'app

--- anssi-nis2-ui/server.cjs
+++ anssi-nis2-ui/server.cjs
@@ -1,10 +1,24 @@
-var express = require('express');
+const express = require('express');
+const basicAuth = require('express-basic-auth')
 
 var app = express();
 var directory = '/' + (process.env.STATIC_DIR || 'dist')
-app.use(express.static(__dirname + directory));
+
+if (process.env.PASSWORD_NIS2_BASIC_AUTH) {
+    var staticUserAuth = basicAuth({
+        users: {
+            'NIS2': process.env.PASSWORD_NIS2_BASIC_AUTH
+        },
+        challenge: true
+    })
+    app.use("/", staticUserAuth, (req, res, next) => {
+        next();
+    })
+}
+app.use("/", express.static(__dirname + directory));
 
 var port = process.env.PORT || 3000;
+
 app.listen(port, function () {
     console.log('Listening on', port);
 });
