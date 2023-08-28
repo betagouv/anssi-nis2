1cf3986b88b7c50b0186300980f5db70fc694e7b
Bastien Gallay <bgallay@palo-it.com>
2023-08-28 19:04:31 +0200

    refacto: etapes

--- anssi-nis2-ui/src/Simulateur.tsx
+++ anssi-nis2-ui/src/Simulateur.tsx
@@ -1,14 +1,19 @@
 import {DefaultComponent} from "./Props.ts"
 import MiseEnPage from "./Components/MiseEnPage.tsx"
-import SimulateurEtape1Localisation from "./Components/Simulateur/SimulateurEtape1Localisation.tsx"
+import {useLoaderData} from "react-router-dom"
+import {ReactNode} from "react"
+
+const ComposantSimulateur:DefaultComponent = ({children}) => children
 
 const Simulateur: DefaultComponent = () => {
+    const etape : ReactNode = useLoaderData() as ReactNode
+
     return <MiseEnPage page={"simulateur"}>
         <div className="fr-container fr-container--fluid fr-mb-md-14v">
             <div className="fr-grid-row fr-grid-row-gutters fr-grid-row--center">
                 <div className="fr-col-12 fr-col-md-10 fr-col-lg-8">
                     <div className="fr-container fr-background-alt--blue-france fr-px-md-0 fr-py-10v fr-py-md-14v">
-                        <SimulateurEtape1Localisation />
+                        <ComposantSimulateur>{etape}</ComposantSimulateur>
                     </div>
                 </div>
             </div>
--- anssi-nis2-ui/src/main.tsx
+++ anssi-nis2-ui/src/main.tsx
@@ -5,23 +5,28 @@ import {
     RouterProvider,
 } from "react-router-dom"
 import Accueil from './Accueil.tsx'
-import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+import {startReactDsfr} from "@codegouvfr/react-dsfr/spa";
 import Simulateur from "./Simulateur.tsx"
-startReactDsfr({ defaultColorScheme: "system" });
+import {loadSimulateur} from "./Utilitaires/LoadSimulateur.tsx"
+
+startReactDsfr({defaultColorScheme: "system"});
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Accueil />
+        element: <Accueil/>,
     },
     {
         path: "/simulateur",
-        element: <Simulateur />
-    }
+        element: <Simulateur/>,
+        loader: loadSimulateur,
+        children: [{path: ":etape"}],
+    },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
