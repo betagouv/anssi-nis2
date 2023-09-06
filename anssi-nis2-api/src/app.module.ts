edb0bf08f902e74775c0e3388f6ece0b9e3d8255
Bastien Gallay <bgallay@palo-it.com>
2023-09-06 10:43:31 +0200

    feat: Database

--- anssi-nis2-api/src/app.module.ts
+++ anssi-nis2-api/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SimulateurController } from './simulateurController';
 import { SimulateurService } from './simulateur.service';
 import { SimulateurDepotToken } from './Domaine/simulateur';
 import { InMemorySimulateurDepot } from './simulateur.depot';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: 'secret',
+      database: 'anssi-nis2',
+      host: 'localhost',
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [SimulateurController],
   providers: [
     SimulateurService,
