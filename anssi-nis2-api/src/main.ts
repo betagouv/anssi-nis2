27b3f4f60eb71746918a81888c456da44761ea51
Bastien Gallay <bgallay@palo-it.com>
2023-09-06 11:36:29 +0200

    feat: Port NestJS pour déploiement

--- anssi-nis2-api/src/main.ts
+++ anssi-nis2-api/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+
+const listeningPort = 3000 || process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(listeningPort);
 }
 
 bootstrap().then(() => '');
