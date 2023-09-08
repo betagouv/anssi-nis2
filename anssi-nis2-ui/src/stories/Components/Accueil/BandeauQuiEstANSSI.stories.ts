67a9d204dd98e961d1b48a97343c18c9b3123f2f
Bastien Gallay <bgallay@palo-it.com>
2023-09-08 15:08:29 +0200

    feat: bandeaux d'accueil dans Storybook

--- anssi-nis2-ui/src/stories/Components/Accueil/BandeauQuiEstANSSI.stories.ts
+++ anssi-nis2-ui/src/stories/Components/Accueil/BandeauQuiEstANSSI.stories.ts
@@ -1,13 +1,13 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { BandeauAccueil } from "../../Components/Accueil";
+import { BandeauQuiEstANSSI } from "../../../Components/Accueil";
 
 //👇 This default export determines where your story goes in the story list
-const meta: Meta<typeof BandeauAccueil> = {
-  component: BandeauAccueil,
+const meta: Meta<typeof BandeauQuiEstANSSI> = {
+  component: BandeauQuiEstANSSI,
 };
 
 export default meta;
-type Story = StoryObj<typeof BandeauAccueil>;
+type Story = StoryObj<typeof BandeauQuiEstANSSI>;
 
 export const FirstStory: Story = {
   args: {
