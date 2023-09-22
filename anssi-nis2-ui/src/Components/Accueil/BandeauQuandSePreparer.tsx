0102db8088a80efcdda31d2be17dc3755c36913a
Bastien Gallay <bgallay@palo-it.com>
2023-09-22 17:43:40 +0200

    feat: affiche nouveau bandeau Quand se préparer

--- anssi-nis2-ui/src/Components/Accueil/BandeauQuandSePreparer.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauQuandSePreparer.tsx
@@ -1,41 +1,14 @@
-import { Props } from "../../Services/Props.ts";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import { noRefClick } from "../../Services/Echaffaudages/AssistantsEchaffaudages.ts";
 import CalendrierOctobre2024 from "../../assets/calendrier-oct-2024.svg";
-import { ColorTheme, useColors } from "@codegouvfr/react-dsfr/useColors";
-import { createMakeAndWithStyles } from "tss-react";
 import styled from "@emotion/styled";
-
-const { makeStyles } = createMakeAndWithStyles({
-  useTheme: function (): ColorTheme {
-    return useColors();
-  },
-});
-const useStyles = makeStyles()(() => ({
-  root: {
-    "& h2": {
-      textTransform: "uppercase",
-      textAlign: "center",
-    },
-    "& .calendrier": {
-      textAlign: "center",
-    },
-  },
-  "mea-faits": {
-    backgroundColor: "var(--light-accent-blue-cumulus-925, #DAE6FD)",
-    width: "fit-content",
-  },
-}));
+import { Link } from "react-router-dom";
 
 const StyledDiv = styled.div`
   border-top: 4px solid var(--light-primary-blue-france-975, #f5f5fe);
   background-color: #f3f6fe;
 `;
-const BandeauQuandSePreparer = (props: Props) => {
-  const { className } = props;
-
-  const { classes, cx } = useStyles();
-
+const BandeauQuandSePreparer = () => {
   return (
     <>
       <StyledDiv className="fr-pt-10w fr-pb-13w">
@@ -52,22 +25,24 @@ const BandeauQuandSePreparer = (props: Props) => {
             </div>
             <div className="fr-col">
               <p className="fr-text--lead fr-text--bold fr-pb-4v">
-                L’administration française a jusqu’au 17 octobre 2024 pour
-                transposer les obligations de cette directive dans le droit
-                national. Son entrée en vigueur, marquant le début des délai de
-                mise en conformité, peut donc être estimée à<br />
-                <span className={cx([classes["mea-faits"]], className)}>
-                  fin de 2e semestre 2024.
-                </span>
+                La France a jusqu’au 17 octobre 2024 pour transposer les
+                obligations de cette directive dans le droit national. Les
+                délais de mise en conformité seront alors précisés et débuteront
+                au plus tard le 18 octobre 2024.
               </p>
               <p className="fr-text--lead  fr-pb-4v">
-                Pour améliorer au plus tôt votre protection face aux menaces
-                cyber et vous préparer à ces futures obligations, suivez-nous
-                dès à présent.
+                Pour estimer rapidement si votre entité peut être concernée et
+                recevoir les futures informations concernant la directive,
+                effectuez dès à présent le test de l’ANSSI.
               </p>
-              <Button className="fr-btn" onClick={noRefClick}>
-                Rester informé
-              </Button>
+              <Link to={"/simulateur"}>
+                <Button
+                  className="fr-btn fr-px-3w fr-pt-1-5v fr-pb-2v"
+                  onClick={noRefClick}
+                >
+                  Débuter le test
+                </Button>
+              </Link>
             </div>
           </div>
         </div>
