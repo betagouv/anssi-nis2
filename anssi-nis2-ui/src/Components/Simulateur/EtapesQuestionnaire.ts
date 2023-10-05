1a98e861593471a4baf8743716d3ceff3121ad5b
Bastien Gallay <bgallay@palo-it.com>
2023-10-05 11:41:33 +0200

    refacto: sépare les déclarations des étapes du questionnaire

--- anssi-nis2-ui/src/Components/Simulateur/EtapesQuestionnaire.ts
+++ anssi-nis2-ui/src/Components/Simulateur/EtapesQuestionnaire.ts
@@ -27,63 +27,74 @@ const validationUneReponses = (
   message: "Selectionnez une réponse",
   validateur: valideAuMoinsUn(nomChamp),
 });
+const validationReponsesTaille = {
+  message: "Sélectionnez une réponse pour chaque critère",
+  validateur: (donneesFormulaire: DonneesFormulaireSimulateur) =>
+    valideAuMoinsUn("trancheNombreEmployes")(donneesFormulaire) &&
+    valideAuMoinsUn("trancheCA")(donneesFormulaire),
+};
+const validationReponsesSecteurs = {
+  message: "Sélectionnez au moins une réponse",
+  validateur: valideAuMoinsUn("secteurActivite"),
+};
+const validationReponsesSousActivites = {
+  message: "Sélectionnez au moins une réponse par secteur",
+  validateur: valideAuMoinsUn("sousSecteurActivite"),
+};
+const validationReponsesActivites = {
+  message: "Sélectionnez au moins une réponse par secteur",
+  validateur: valideAuMoinsUn("activites"),
+};
+const informationEtapeDesignationOSE = new InformationEtapeForm(
+  "Désignation éventuelle",
+  validationUneReponses("designeOSE"),
+  EtapeOSE,
+);
+const informationEtapeLocalisation = new InformationEtapeForm(
+  "Localisation de l’activité",
+  validationUneReponses("etatMembre"),
+  EtapeLocalisation,
+);
+const informationEtapeTypeStructure = new InformationEtapeForm(
+  "Type de structure",
+  validationUneReponses("typeStructure"),
+  EtapeTypeStructure,
+);
+const informationEtapeTaille = new InformationEtapeForm(
+  "Taille de l’organisation",
+  validationReponsesTaille,
+  EtapeTaille,
+);
+const informationEtapeSousSecteurs = new InformationEtapeForm(
+  "Sous-secteur d'activité",
+  validationReponsesSousActivites,
+  EtapeSousSecteurActivite,
+);
+const informationEtapeSecteurs = new InformationEtapeForm(
+  "Secteurs d’activité",
+  validationReponsesSecteurs,
+  EtapeSecteurActivite,
+  new SousEtapeConditionnelle(
+    ({ secteurActivite }) => secteurActivite.includes("energie"),
+    informationEtapeSousSecteurs,
+  ),
+);
+const informationEtapeActivites = new InformationEtapeForm(
+  "Activités pratiquées",
+  validationReponsesActivites,
+  EtapeActivite,
+);
 export const etapesQuestionnaire: CollectionInformationsEtapes =
   new CollectionInformationsEtapes(
-    new InformationEtapeForm(
-      "Désignation éventuelle",
-      validationUneReponses("designeOSE"),
-      EtapeOSE,
-    ),
+    informationEtapeDesignationOSE,
 
-    new InformationEtapeForm(
-      "Localisation de l’activité",
-      validationUneReponses("etatMembre"),
-      EtapeLocalisation,
-    ),
+    informationEtapeLocalisation,
 
-    new InformationEtapeForm(
-      "Type de structure",
-      validationUneReponses("typeStructure"),
-      EtapeTypeStructure,
-    ),
+    informationEtapeTypeStructure,
 
-    new InformationEtapeForm(
-      "Taille de l’organisation",
-      {
-        message: "Sélectionnez une réponse pour chaque critère",
-        validateur: (donneesFormulaire: DonneesFormulaireSimulateur) =>
-          valideAuMoinsUn("trancheNombreEmployes")(donneesFormulaire) &&
-          valideAuMoinsUn("trancheCA")(donneesFormulaire),
-      },
-      EtapeTaille,
-    ),
-    new InformationEtapeForm(
-      "Secteurs d’activité",
-      {
-        message: "Sélectionnez au moins une réponse",
-        validateur: valideAuMoinsUn("secteurActivite"),
-      },
-      EtapeSecteurActivite,
-      new SousEtapeConditionnelle(
-        ({ secteurActivite }) => secteurActivite.includes("energie"),
-        new InformationEtapeForm(
-          "Sous-secteur d'activité",
-          {
-            message: "Sélectionnez au moins une réponse par secteur",
-            validateur: valideAuMoinsUn("sousSecteurActivite"),
-          },
-          EtapeSousSecteurActivite,
-        ),
-      ),
-    ),
-    new InformationEtapeForm(
-      "Activités pratiquées",
-      {
-        message: "Sélectionnez au moins une réponse par secteur",
-        validateur: valideAuMoinsUn("activites"),
-      },
-      EtapeActivite,
-    ),
+    informationEtapeTaille,
+    informationEtapeSecteurs,
+    informationEtapeActivites,
     new InformationEtapeResult("Resultat"),
   );
 export const etatEtapesInitial = new EtatEtapes(etapesQuestionnaire, 1);
