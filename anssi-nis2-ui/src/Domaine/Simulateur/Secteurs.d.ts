3600c5eb712e565bb69f1b2032c705e60f284e3f
Bastien Gallay <bgallay@palo-it.com>
2023-10-09 16:04:50 +0200

    refacto: supprime déclarations inutiles

--- anssi-nis2-ui/src/Domaine/Simulateur/Secteurs.d.ts
+++ anssi-nis2-ui/src/Domaine/Simulateur/Secteurs.d.ts
@@ -1,17 +1,9 @@
 import {
-  TValeursActivites,
   TValeursSousSecteurEnergie,
   TValeursSousSecteurFabrication,
-  TValeursSousSecteursActivites,
   TValeursSousSecteurTransport,
 } from "./ValeursCles.ts";
 
-export type ListeActivites<T extends TValeursActivites> = Record<T, string>;
-export type DescriptionSousSecteur = {
-  libelle: string;
-  activites: ListeActivites;
-};
-export type DetailsSousSecteurs = Record<TValeursSousSecteursActivites, string>;
 export type DetailsSousSecteurUnique<
   T extends
     | TValeursSousSecteurEnergie
