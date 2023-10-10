ddaaa28d3d3e3ed0b289229ca4055e98ff3ff2cd
Bastien Gallay <bgallay@palo-it.com>
2023-10-10 18:30:47 +0200

    feat: affiche les informations pour non-éligibilité

--- anssi-nis2-ui/src/References/contenusResultatEligibilite.ts
+++ anssi-nis2-ui/src/References/contenusResultatEligibilite.ts
@@ -38,10 +38,19 @@ export const contenusResultatNonEligible: ContenusResultatEligibilite = {
   pointsAttention: [
     {
       description:
-        "Sous réserve des mécanismes d'exemption ou de désignation pouvant " +
-        "être mis en place au cas par cas par le gouvernement français pour " +
-        "certaines entités. Ces exemptions ou désignation seront connues au " +
-        "plus tard le 18 octobre 2024.",
+        "Ce résultat est présenté au vu des éléments saisis et sous réserve " +
+        "des mécanismes d'exemption ou de désignation pouvant être mis en " +
+        "place au cas par cas par le gouvernement français pour certaines " +
+        "entités. Ces exemptions ou désignations seront connues au plus " +
+        "tard le 18 octobre 2024.",
+    },
+    {
+      titre: "Critères de possible inclusion",
+      description:
+        " - l’entité est, dans un État membre, le seul prestataire d’un service qui est essentiel au maintien d’activités sociétales ou économiques critiques;" +
+        " - une perturbation du service fourni par l’entité pourrait avoir un impact important sur la sécurité publique, la sûreté publique ou la santé publique ;" +
+        " - une perturbation du service fourni par l’entité pourrait induire un risque systémique important, en particulier pour les secteurs où cette perturbation pourrait avoir un impact transfrontière ;" +
+        " - l’entité est critique en raison de son importance spécifique au niveau national ou régional pour le secteur ou le type de service en question, ou pour d’autres secteurs interdépendants dans l’État membre ;",
     },
   ],
 };
