628004f759004b21d6d4253c9ce716feb07cf199
Bastien Gallay <bgallay@palo-it.com>
2023-10-10 10:24:02 +0200

    refacto: tests: ajoute le coverage à vitest et SB-test

--- anssi-nis2-ui/.storybook/main.ts
+++ anssi-nis2-ui/.storybook/main.ts
@@ -21,6 +21,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
+    "@storybook/addon-coverage",
   ],
   docs: {
     autodocs: "tag",
