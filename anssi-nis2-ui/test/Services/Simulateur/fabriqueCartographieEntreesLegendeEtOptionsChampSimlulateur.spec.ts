330b3639eb41bcc87a94c74e79d83f48e3019ea2
Bastien Gallay <bgallay@palo-it.com>
2023-10-19 09:42:48 +0200

    refacto: nettoie le code du test fabriqueCartographieEntreesLegendeEtOptionsChampSimlulateur.spec.ts

--- anssi-nis2-ui/test/Services/Simulateur/fabriqueCartographieEntreesLegendeEtOptionsChampSimlulateur.spec.ts
+++ anssi-nis2-ui/test/Services/Simulateur/fabriqueCartographieEntreesLegendeEtOptionsChampSimlulateur.spec.ts
@@ -1,26 +1,12 @@
 import { describe, it, expect } from "vitest";
 import { fc } from "@fast-check/vitest";
 import { fabriqueCartographieEntreesLegendeEtOptionsChampSimlulateur } from "../../../src/Services/Simulateur/Transformateurs/FabriqueConstructeurOptionActivite";
-import {
-  arbForm,
-  arbTrancheSingleton,
-  fabriqueArbContraintSurTrancheCA,
-} from "../../Domaine/arbitraires/arbitrairesSimulateur";
 import { IDonneesFormulaireSimulateur } from "../../../src/Domaine/Simulateur/DonneesFormulaire";
 import {
-  ajouteArbitraireActivites,
   ajouteMethodeAvec,
-  DonneesSansActivite,
   fabriqueArbSecteurSousSecteurs,
-  propageBase,
 } from "../../utilitaires/manipulationArbitraires";
 import { listeEnrSecteursAvecLeursSousSecteurs } from "../../../src/Domaine/Simulateur/ListesEnrSecteursSousSecteur";
-import {
-  arbAppartenancePaysUnionEuropeenne,
-  arbDesigneOperateurServicesEssentiels,
-  arbTypeStructure,
-} from "../../Domaine/arbitraires/arbitraireChampFormulaire";
-import { estActiviteAutre } from "../../../src/Domaine/Simulateur/Operations/FiltreActivites";
 
 const donneesArbitrairesFormNonOSEPrivesMoyenneGrandeAutresActivites: fc.Arbitrary<IDonneesFormulaireSimulateur> =
   fabriqueArbSecteurSousSecteurs(listeEnrSecteursAvecLeursSousSecteurs, {
