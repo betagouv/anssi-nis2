76e287d7d6ab060eb9733ad291132377ee1e6dcc
Bastien Gallay <bgallay@palo-it.com>
2023-10-20 15:36:06 +0200

    feat: intègre l'étape préalable "Pour bien débuter" - fin

--- anssi-nis2-ui/src/Components/Simulateur/SimulateurEtapePrealable.tsx
+++ anssi-nis2-ui/src/Components/Simulateur/SimulateurEtapePrealable.tsx
@@ -1,26 +1,40 @@
 import { SimulateurEtapeRenderedComponent } from "../../Services/Simulateur/Props/component";
 import { RowContainer } from "../General/RowContainer.tsx";
 import { CenteredContainer } from "../General/CenteredContainer.tsx";
+import { Button } from "@codegouvfr/react-dsfr/Button";
+import { SimulateurEtapeRenderedProps } from "../../Services/Simulateur/Props/simulateurEtapeProps";
 
-export const SimulateurEtapePrealable: SimulateurEtapeRenderedComponent =
-  () => (
-    <>
-      <RowContainer className="fr-py-3w">
-        <CenteredContainer className="fr-background-alt--grey fr-nis2-bien-debuter">
-          <h2>Pour bien débuter</h2>
-          <div>
-            <p>
-              Ce test permet de cerner si votre entité sera concernée par la
-              directive NIS 2, et ce dès 2024. Nous le tiendrons à jour au fil
-              des précisions apportées par la transposition en droit français.
-            </p>
-            <p className="fr-text--bold">
-              Si votre entité appartient à un groupe d’entreprises aux activités
-              variées, ce test peut être réalisé pour chaque entreprise filiale,
-              ou pour le groupe si celui-ci mène une activité économique.
-            </p>
-          </div>
-        </CenteredContainer>
-      </RowContainer>
-    </>
-  );
+export const SimulateurEtapePrealable: SimulateurEtapeRenderedComponent = ({
+  informationsBoutonsNavigation,
+}: SimulateurEtapeRenderedProps) => (
+  <>
+    <RowContainer className="fr-py-3w">
+      <CenteredContainer className="fr-background-alt--grey fr-nis2-bien-debuter">
+        <h2>Pour bien débuter</h2>
+        <div className="fr-pr-16w">
+          <p>
+            Ce test permet de cerner si votre entité sera concernée par la
+            directive NIS 2, et ce dès 2024. Nous le tiendrons à jour au fil des
+            précisions apportées par la transposition en droit français.
+          </p>
+          <p className="fr-text--bold">
+            Si votre entité appartient à un groupe d’entreprises aux activités
+            variées, ce test peut être réalisé pour chaque entreprise filiale,
+            ou pour le groupe si celui-ci mène une activité économique.
+          </p>
+        </div>
+        <div className="fr-btns-group--right">
+          <Button
+            onClick={informationsBoutonsNavigation.precedent}
+            priority="tertiary"
+          >
+            Revenir à l&apos;accueil
+          </Button>
+          <Button onClick={informationsBoutonsNavigation.suivant}>
+            Débuter le test
+          </Button>
+        </div>
+      </CenteredContainer>
+    </RowContainer>
+  </>
+);
--- anssi-nis2-ui/src/stories/SimulateurEtapePrealable.stories.tsx
+++ anssi-nis2-ui/src/stories/SimulateurEtapePrealable.stories.tsx
@@ -1,9 +1,15 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { SimulateurEtapePrealable } from "../Components/Simulateur/SimulateurEtapePrealable";
+import { pageDecorator } from "./utilitaires/PageDecorator.tsx";
+import { jest } from "@storybook/jest";
+import { EtatEtapes } from "../Services/Simulateur/EtatEtapes.ts";
+import { CollectionInformationsEtapes } from "../Services/Simulateur/CollectionInformationsEtapes.ts";
+import { donneesFormulaireSimulateurVide } from "../Domaine/Simulateur/DonneesFormulaire.ts";
 
 const meta = {
   title: "EtapeSousSecteursActivite/SimulateurEtapePrealable",
   component: SimulateurEtapePrealable,
+  decorators: [pageDecorator],
 } satisfies Meta<typeof SimulateurEtapePrealable>;
 
 export default meta;
@@ -11,5 +17,15 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const SimulateurEtapePrealableStory: Story = {
+  args: {
+    informationsBoutonsNavigation: {
+      precedent: jest.fn(),
+      suivant: jest.fn(),
+    },
+    etatEtapes: jest.mocked(
+      new EtatEtapes(new CollectionInformationsEtapes(...[]), 0),
+    ),
+    donneesFormulaire: jest.mocked(donneesFormulaireSimulateurVide),
+  },
   name: "Default",
 };
