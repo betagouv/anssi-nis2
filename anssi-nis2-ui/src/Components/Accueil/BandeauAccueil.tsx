cbc9eef08c0a7f850bb08219b7b3fd9b7d8f4127
GalloIT <bgallay@palo-it.com>
2023-11-09 17:38:04 +0100

    feat: intègre nouveau design accueil haut

--- anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
@@ -1,48 +1,31 @@
-import FondHabillageAccueil from "../../assets/habillage-accueil-01.svg";
-import { fr } from "@codegouvfr/react-dsfr";
-import { CoolIllustration } from "../Echaffaudages/CoolIllustration.tsx";
-import styled from "@emotion/styled";
 import { DefaultComponent } from "../../Services/Props";
-
-const BandeauAccueilNis2 = styled.div`
-  background: #f3f6fe url(${FondHabillageAccueil}) no-repeat bottom right;
-`;
-
-const AppelAction = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${fr.spacing("2w")};
-`;
-
-const MiseEnAvant = styled.p`
-  background-color: var(--light-accent-green-emeraude-950, #c3fad5);
-  width: fit-content;
-`;
+import { RowContainer } from "../General/RowContainer.tsx";
 
 const BandeauAccueil: DefaultComponent = () => {
   return (
-    <BandeauAccueilNis2>
-      <div className="fr-container">
-        <div className="fr-grid-row fr-pt-12w fr-pb-8w">
-          <AppelAction className="fr-col">
-            <div>
-              <h1 className="fr-text-title--blue-france fr-mb-0">
-                MonEspaceNIS2
-              </h1>
-              <p className="fr-text--lead">
-                Accompagner les organisations dans la compréhension
-                <br />
-                et la mise en conformité à NIS&nbsp;2
-              </p>
+    <div className="fr-py-5w  fr-bandeau-marianne fr-nis2-accueil-principal">
+      <div className="fr-my-0 fr-mx-auto">
+        <RowContainer>
+          <div className="fr-container">
+            <div className="fr-grid-row fr-pt-12w fr-pb-8w">
+              <div className="fr-col fr-nis2-appel-action">
+                <div>
+                  <h1>MonEspaceNIS2</h1>
+                  <p>
+                    Accompagner les organisations dans la compréhension
+                    <br />
+                    et la mise en conformité à NIS&nbsp;2
+                  </p>
+                </div>
+                <p className="fr-nis2-mise-en-avant">
+                  Gratuit et 100% en ligne
+                </p>
+              </div>
             </div>
-            <MiseEnAvant className="fr-text--bold fr-text--lg fr-px-1v fr-py-0-5v">
-              Gratuit et 100% en ligne
-            </MiseEnAvant>
-          </AppelAction>
-          <CoolIllustration />
-        </div>
+          </div>
+        </RowContainer>
       </div>
-    </BandeauAccueilNis2>
+    </div>
   );
 };
 
