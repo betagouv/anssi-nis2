b827fcf05d3f7fff515ab78874aa48fb7137bc00
GalloIT <bgallay@palo-it.com>
2023-11-09 19:33:24 +0100

    feat: intègre nouveau design pour "Quand se préparer"

--- anssi-nis2-ui/src/Components/Accueil/BandeauQuandSePreparer.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauQuandSePreparer.tsx
@@ -1,17 +1,12 @@
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import { noRefClick } from "../../Services/Echaffaudages/AssistantsEchaffaudages.tsx";
 import CalendrierOctobre2024 from "../../assets/calendrier-oct-2024.svg";
-import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 
-const StyledDiv = styled.div`
-  border-top: 4px solid var(--light-primary-blue-france-975, #f5f5fe);
-  background-color: #f3f6fe;
-`;
 const BandeauQuandSePreparer = () => {
   return (
     <>
-      <StyledDiv className="fr-pt-10w fr-pb-13w">
+      <div className="fr-bandeau-marianne fr-nis2-quand-se-preparer fr-pt-10w fr-pb-13w">
         <div className={"fr-container"}>
           <div className="fr-grid-row--center">
             <h2 className="fr-mb-7w">Quand se préparer ?</h2>
@@ -24,20 +19,21 @@ const BandeauQuandSePreparer = () => {
               />
             </div>
             <div className="fr-col">
-              <p className="fr-text--lead fr-text--bold fr-pb-4v">
-                La France a jusqu’au 17 octobre 2024 pour transposer les
-                obligations de cette directive dans le droit national. Les
-                délais de mise en conformité seront alors précisés et débuteront
-                au plus tard le 18 octobre 2024.
+              <p className="fr-text--lead fr-pb-4v">
+                La France a jusqu’au{" "}
+                <span className="fr-text--bold">17 octobre 2024</span> pour
+                transposer les obligations de cette directive dans le droit
+                national. Les délais de mise en conformité seront alors précisés
+                et débuteront au plus tard le 18 octobre 2024.
               </p>
-              <p className="fr-text--lead  fr-pb-4v">
+              <p className="fr-text--lead fr-text--bold fr-pb-4v">
                 Pour estimer rapidement si votre entité peut être concernée et
-                recevoir les futures informations concernant la directive,
-                effectuez dès à présent le test de l’ANSSI.
+                recevoir des informations concernant la directive, effectuez dès
+                à présent une simulation de l’ANSSI.
               </p>
               <Link to={"/simulateur"}>
                 <Button
-                  className="fr-btn fr-px-3w fr-pt-1-5v fr-pb-2v"
+                  className="fr-btn fr-nis2-mise-en-avant fr-px-3w fr-pt-1-5v fr-pb-2v"
                   onClick={noRefClick}
                 >
                   Débuter le test
@@ -46,7 +42,7 @@ const BandeauQuandSePreparer = () => {
             </div>
           </div>
         </div>
-      </StyledDiv>
+      </div>
     </>
   );
 };
