9e0be87ed36808e7ac21fa3f7d34cdad771f3ab7
GalloIT <bgallay@palo-it.com>
2023-11-15 14:27:46 +0100

    feat: ajoute le service d'enregistrement des informations d'email (Controller)

--- anssi-nis2-api/src/informations-emails/informations-emails.controller.spec.ts
+++ anssi-nis2-api/src/informations-emails/informations-emails.controller.spec.ts
@@ -3,6 +3,8 @@ import { InformationsEmailsController } from "./informations-emails.controller";
 import { InformationsEmailsService } from "./informations-emails.service";
 import { provideInformationsEmailRepositoryKey } from "../constantes.ts";
 import { mockInformationsEmailRepository } from "./fabrique-mock.repository.ts";
+import { CreateInformationsEmailDto } from "./dto/create-informations-email.dto.ts";
+import { espereEmailsInformationCorrespondASonDto } from "./helpers/testHelpers.ts";
 
 describe("InformationsEmailsController", () => {
   let controller: InformationsEmailsController;
@@ -24,7 +26,14 @@ describe("InformationsEmailsController", () => {
     );
   });
 
-  it("should be defined", () => {
-    expect(controller).toBeDefined();
+  it("doit retourner l'identifiant", async () => {
+    const informationsEmail: CreateInformationsEmailDto = {
+      email: "toto@titi.tutu",
+      accepteInfolettreNis2: true,
+      accepteInfolettreServicesDedies: true,
+      nomOrganisation: "Titi International",
+    };
+    const valeurAjoutee = await controller.ajoute(informationsEmail);
+    espereEmailsInformationCorrespondASonDto(valeurAjoutee, informationsEmail);
   });
 });
--- anssi-nis2-api/src/informations-emails/informations-emails.service.spec.ts
+++ anssi-nis2-api/src/informations-emails/informations-emails.service.spec.ts
@@ -2,26 +2,11 @@ import { Test, TestingModuleBuilder } from "@nestjs/testing";
 import { InformationsEmailsService } from "./informations-emails.service";
 import { CreateInformationsEmailDto } from "./dto/create-informations-email.dto.ts";
 import { provideInformationsEmailRepositoryKey } from "../constantes.ts";
-import { InformationsEmail } from "./entities/informations-email.entity.ts";
 import { DatabaseModule } from "../database/database.module.ts";
 import { ConfigModule } from "@nestjs/config";
 import { informationsEmailsProviders } from "./informations-emails.providers.ts";
 import { mockInformationsEmailRepository } from "./fabrique-mock.repository.ts";
-
-function espereEmailsInformationCorrespondASonDto(
-  reponse: InformationsEmail,
-  informationsEmail: CreateInformationsEmailDto,
-) {
-  expect(reponse.id).toBeDefined();
-  expect(reponse.email).toBe(informationsEmail.email);
-  expect(reponse.accepteInfolettreNis2).toBe(
-    informationsEmail.accepteInfolettreNis2,
-  );
-  expect(reponse.accepteInfolettreServicesDedies).toBe(
-    informationsEmail.accepteInfolettreServicesDedies,
-  );
-  expect(reponse.nomOrganisation).toBe(informationsEmail.nomOrganisation);
-}
+import { espereEmailsInformationCorrespondASonDto } from "./helpers/testHelpers.ts";
 
 describe("InformationsEmailsService", () => {
   const testingModuleBuilder = Test.createTestingModule({
