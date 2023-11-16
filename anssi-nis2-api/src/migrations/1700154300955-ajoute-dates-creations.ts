af4bf95d1539c8ce77d2c47b154a5b231163e6ec
GalloIT <bgallay@palo-it.com>
2023-11-16 19:08:32 +0100

    fix: crée les tables si elles n'existent pas pour la première migration

--- anssi-nis2-api/src/migrations/1700154300955-ajoute-dates-creations.ts
+++ anssi-nis2-api/src/migrations/1700154300955-ajoute-dates-creations.ts
@@ -1,24 +1,80 @@
-import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableColumn } from "typeorm";
 
 export class AjouteDatesCreations1700154300955 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const dateMigration = `NOW()`;
-    await queryRunner.addColumn(
-      "simulateur_reponse",
-      new TableColumn({
-        name: "dateCreation",
-        type: "timestamp",
-        default: dateMigration,
-      }),
-    );
-    await queryRunner.addColumn(
-      "informations_email",
-      new TableColumn({
-        name: "dateCreation",
-        type: "timestamp",
-        default: dateMigration,
-      }),
-    );
+    const simulateurReponse = "simulateur_reponse";
+    const colonneTimeStamp = {
+      name: "dateCreation",
+      type: "timestamp",
+      default: dateMigration,
+    };
+    if (!(await queryRunner.hasTable(simulateurReponse))) {
+      await queryRunner.createTable(
+        new Table({
+          name: simulateurReponse,
+          columns: [
+            {
+              name: "id",
+              type: "int",
+              isPrimary: true,
+              isGenerated: true,
+            },
+            {
+              name: "reponseJson",
+              type: "varchar",
+            },
+            colonneTimeStamp,
+          ],
+        }),
+      );
+    } else {
+      await queryRunner.addColumn(
+        simulateurReponse,
+        new TableColumn(colonneTimeStamp),
+      );
+    }
+
+    const informationsEmail = "informations_email";
+    if (!(await queryRunner.hasTable(informationsEmail))) {
+      await queryRunner.createTable(
+        new Table({
+          name: informationsEmail,
+          columns: [
+            {
+              name: "id",
+              type: "int",
+              isPrimary: true,
+              isGenerated: true,
+            },
+            {
+              name: "email",
+              type: "varchar",
+            },
+            {
+              name: "nomOrganisation",
+              type: "varchar",
+              isNullable: true,
+            },
+            {
+              name: "accepteInfolettreNis2",
+              type: "bool",
+              default: false,
+            },
+            {
+              name: "accepteInfolettreServicesDedies",
+              type: "bool",
+              default: false,
+            },
+          ],
+        }),
+      );
+    } else {
+      await queryRunner.addColumn(
+        informationsEmail,
+        new TableColumn(colonneTimeStamp),
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
