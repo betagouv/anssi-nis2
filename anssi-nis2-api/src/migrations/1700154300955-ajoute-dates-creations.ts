5a8ad11de5b0619679b983c0a0cde8385eed2326
GalloIT <bgallay@palo-it.com>
2023-11-16 18:30:30 +0100

    feat: ajoute les migrations

---
+++ anssi-nis2-api/src/migrations/1700154300955-ajoute-dates-creations.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AjouteDatesCreations1700154300955 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const dateMigration = `NOW()`;
+    await queryRunner.addColumn(
+      "simulateur_reponse",
+      new TableColumn({
+        name: "dateCreation",
+        type: "timestamp",
+        default: dateMigration,
+      }),
+    );
+    await queryRunner.addColumn(
+      "informations_email",
+      new TableColumn({
+        name: "dateCreation",
+        type: "timestamp",
+        default: dateMigration,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("simulateur_reponse", "createdDate");
+    await queryRunner.dropColumn("informations_email", "createdDate");
+  }
+}
