50b8e1df0b89abb9102fde804c4193edc2d27c08
GalloIT <bgallay@palo-it.com>
2023-11-17 16:53:22 +0100

    feat: Affiche le bon formulaire email selon le résultat obtenu

--- anssi-nis2-ui/src/Components/Simulateur/Resultats/LigneResterInformer.tsx
+++ anssi-nis2-ui/src/Components/Simulateur/Resultats/LigneResterInformer.tsx
@@ -1,11 +1,17 @@
 import { RowContainer } from "../../General/RowContainer.tsx";
 import { CenteredContainer } from "../../General/CenteredContainer.tsx";
 import RestezInformes from "../../RestezInformes.tsx";
+import {
+  DefaultComponentExtensible,
+  RestezInformesProps,
+} from "../../../Services/Props";
 
-export const LigneResterInformer = () => (
+export const LigneResterInformer: DefaultComponentExtensible<
+  RestezInformesProps
+> = ({ mode = "simple" }: RestezInformesProps) => (
   <RowContainer className="fr-background-alt--blue-france">
     <CenteredContainer>
-      <RestezInformes />
+      <RestezInformes mode={mode} />
     </CenteredContainer>
   </RowContainer>
 );
--- anssi-nis2-ui/src/Services/Simulateur/Props/contenusResultatEligibilite.d.ts
+++ anssi-nis2-ui/src/Services/Simulateur/Props/contenusResultatEligibilite.d.ts
@@ -6,6 +6,7 @@ export type ContenusResultatEligibilite = {
   titre: string;
   sousTitre?: string;
   classIcone?: string;
+  modeFormulaireEmail: "simple" | "complet";
   classeDivResultat: string;
   fichierPrecisionSurReponse?: string;
   afficheBlocs: Record<BlocResultatSpecifiques, boolean>;
