1e14342bb7ba757550ede8c77c63075e651cb482
GalloIT <bgallay@palo-it.com>
2023-11-24 10:12:15 +0100

    wip: Simplifie les injections de repositories -- Tests InformationsEmails

--- anssi-nis2-api/src/informations-emails/informations-emails.service.spec.ts
+++ anssi-nis2-api/src/informations-emails/informations-emails.service.spec.ts
@@ -8,102 +8,65 @@ import { espereEmailsInformationCorrespondASonDto } from "./helpers/testHelpers"
 import { InformationsEmail } from "./entities/informations-email.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { databaseProviders } from "../database/database.providers";
-import { DataSource, Repository } from "typeorm";
-import { fabriqueAppDataSource } from "../app-data-source.fabrique";
-import {
-  fabriqueConfigService,
-  fabriqueModuleTestAvecFauxServiceConfig,
-} from "../test/utilitaires/helpers";
 import { fabriqueAsynchroneOptionsTypeOrm } from "../Fabriques/fabriqueAsynchroneOptionsTypeOrm";
+import "dotenv/config";
+import { env } from "process";
+import { Provider } from "@nestjs/common";
 
-describe("InformationsEmailsService", () => {
-  // const testingModuleBuilder = Test.createTestingModule({
-  //   providers: [
-  //     InformationsEmailsService,
-  //     {
-  //       provide: datasourceKey,
-  //       useValue: mockInformationsEmailRepository,
-  //     },
-  //   ],
-  // });
-  // const testingModuleConcretBuilder = Test.createTestingModule({
-  //   imports: [TypeOrmModule.forFeature([InformationsEmail])],
-  //   providers: [
-  //     InformationsEmailsService,
-  //     {
-  //       provide: datasourceKey,
-  //       useValue: new DataSource({
-  //         type: "postgres",
-  //         url: "postgres://postgres:secret@localhost/anssi-nis2",
-  //         entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-  //         subscribers: [],
-  //         migrations: [],
-  //       }),
-  //     },
-  //   ],
-  // });
-  let newService: InformationsEmailsService;
+const fabriqueService = async (m: TestingModuleBuilder) =>
+  (await m.compile()).get<InformationsEmailsService>(InformationsEmailsService);
 
-  beforeEach(async () => {
-    const valeursChaineConfiguration = {
-      SCALINGO_POSTGRESQL_URL:
-        "postgres://postgres:secret@localhost/anssi-nis2",
-    };
-    const fauxServiceConfiguration = {
-      provide: ConfigService,
-      useValue: {
-        get: jest.fn((cleDeConfiguration: string, valeurParDefaut: string) => {
-          return (
-            valeursChaineConfiguration[cleDeConfiguration] || valeurParDefaut
-          );
-        }),
-      },
-    };
-    const configService = await fabriqueModuleTestAvecFauxServiceConfig(
-      valeursChaineConfiguration,
-    );
-    const moduleRef = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRootAsync(fabriqueAsynchroneOptionsTypeOrm()),
-        TypeOrmModule.forFeature([InformationsEmail]),
-        ConfigModule.forRoot({
-          isGlobal: true,
-        }),
-      ],
-      providers: [
-        ...databaseProviders,
-        InformationsEmailsService,
-        fauxServiceConfiguration,
-      ],
-    }).compile();
-
-    newService = moduleRef.get<InformationsEmailsService>(
-      InformationsEmailsService,
-    );
+const fabriqueConstructeurTestModule = (fournisseurs: Provider[]) =>
+  Test.createTestingModule({
+    imports: [
+      TypeOrmModule.forRootAsync(fabriqueAsynchroneOptionsTypeOrm()),
+      TypeOrmModule.forFeature([InformationsEmail]),
+      ConfigModule.forRoot({
+        isGlobal: true,
+      }),
+    ],
+    providers: [...fournisseurs, InformationsEmailsService],
   });
-  // const fabriqueService = async (m: TestingModuleBuilder) =>
-  //   (await m.compile()).get<InformationsEmailsService>(
-  //     InformationsEmailsService,
-  //   );
-  const informationsEmail: CreateInformationsEmailDto = {
-    email: "toto@titi.tutu",
-    accepteInfolettreNis2: true,
-    accepteInfolettreServicesDedies: true,
-    nomOrganisation: "Titi International",
-  };
+const informationsEmail: CreateInformationsEmailDto = {
+  email: "toto@titi.tutu",
+  accepteInfolettreNis2: true,
+  accepteInfolettreServicesDedies: true,
+  nomOrganisation: "Titi International",
+};
+describe("InformationsEmailsService", () => {
+  const testingModuleBuilder = fabriqueConstructeurTestModule([
+    {
+      provide: datasourceKey,
+      useValue: mockInformationsEmailRepository,
+    },
+  ]);
 
-  // it("should be defined", async () => {
-  //   expect(await fabriqueService(testingModuleBuilder)).toBeDefined();
-  // });
-  //
-  // it("ajoute les donnees dans la base mockée", async () => {
-  //   const srv = await fabriqueService(testingModuleBuilder);
-  //   const reponse = await srv.ajoute(informationsEmail);
-  //   espereEmailsInformationCorrespondASonDto(reponse, informationsEmail);
-  // });
+  it("ajoute les donnees dans la base mockée", async () => {
+    const srv = await fabriqueService(testingModuleBuilder);
+    const reponse = await srv.ajoute(informationsEmail);
+    espereEmailsInformationCorrespondASonDto(reponse, informationsEmail);
+  });
+});
+
+describe("InformationsEmailsService sur vraie DB", () => {
+  const valeursChaineConfiguration = env;
+  const fauxServiceConfiguration: Provider = {
+    provide: ConfigService,
+    useValue: {
+      get: jest.fn((cleDeConfiguration: string, valeurParDefaut: string) => {
+        return (
+          valeursChaineConfiguration[cleDeConfiguration] || valeurParDefaut
+        );
+      }),
+    },
+  };
+  const testingModuleBuilder = fabriqueConstructeurTestModule([
+    ...databaseProviders,
+    fauxServiceConfiguration,
+  ]);
 
   it("ajoute les donnees dans la base réelle", async () => {
-    const srv = newService; //await fabriqueService(testingModuleConcretBuilder);
+    const srv = await fabriqueService(testingModuleBuilder);
     const reponse = await srv.ajoute(informationsEmail);
     espereEmailsInformationCorrespondASonDto(reponse, informationsEmail);
   });
