6de92ce3e552dfcc4e4fee9d9db02ac6f18589f3
GalloIT <bgallay@palo-it.com>
2023-11-29 14:30:14 +0100

    clean: applique Prettier

--- anssi-nis2-ui/src/stories/utilitaires/mocks.ts
+++ anssi-nis2-ui/src/stories/utilitaires/mocks.ts
@@ -1,24 +1,24 @@
 import { jest } from "@storybook/jest";
-import { IDonneesBrutesFormulaireSimulateur } from "../../../../anssi-nis2-domain/src/Simulateur/DonneesFormulaire";
+import { IDonneesBrutesFormulaireSimulateur } from "anssi-nis2-domain/src/Simulateur/DonneesFormulaire.ts";
 import {
   AggregatInformationsEmail,
-  InformationsEmail
-} from "../../../../anssi-nis2-domain/src/Contact/InformationsEmail.definitions";
+  InformationsEmail,
+} from "anssi-nis2-domain/src/Contact/InformationsEmail.definitions.ts";
 
 export const mockSendFormData = jest.fn(
   async (donnees: IDonneesBrutesFormulaireSimulateur) => {
     const donneesEnvoyees = JSON.stringify(donnees);
     console.log("Appel de mockSendFormData avec arguments: ", donneesEnvoyees);
     return donneesEnvoyees;
-  }
+  },
 );
 export const mockEnregistreInformationsEmail = jest.fn(
   async (informations: InformationsEmail) => {
     const donneesEnvoyees = JSON.stringify(informations);
     console.log(
       "Appel de mockEnregistreInformationsEmail avec arguments: ",
-      donneesEnvoyees
+      donneesEnvoyees,
     );
     return { ...informations, id: 0 } as AggregatInformationsEmail;
-  }
+  },
 );
