4ee341d7b36f412e66e8c55d44d3eb0e6acf5dbf
GalloIT <bgallay@palo-it.com>
2023-12-04 14:54:34 +0100

    refacto: Externalise le opérations Sectorisation

--- anssi-nis2-api/src/journal/journal.service.ts
+++ anssi-nis2-api/src/journal/journal.service.ts
@@ -3,48 +3,8 @@ import { Evenements } from "./entites/evenements.entite-journal";
 import { Injectable } from "@nestjs/common";
 import { DataSource, Repository } from "typeorm";
 import { IDonneesBrutesFormulaireSimulateur } from "anssi-nis2-ui/src/Domaine/Simulateur/DonneesFormulaire";
+import { extraitSectorisationDonneesSimulateur } from "anssi-nis2-ui/src/Domaine/Simulateur/services/Sectorisation/Sectorisation.operations";
 import { SegmentsConcernesNis2 } from "./entites/segments-concernes-nis2.entite-journal";
-import { SecteurActivite } from "anssi-nis2-ui/src/Domaine/Simulateur/SecteurActivite.definitions";
-import {
-  SecteursAvecSousSecteurs,
-  SousSecteurActivite,
-} from "anssi-nis2-ui/src/Domaine/Simulateur/SousSecteurActivite.definitions";
-import { estDansSecteur } from "anssi-nis2-ui/src/Domaine/Simulateur/services/SousSecteurActivite/SousSecteurActivite.predicats";
-import { estUnSecteurSansSousSecteur } from "anssi-nis2-ui/src/Domaine/Simulateur/services/SecteurActivite/SecteurActivite.predicats";
-import { Sectorisation } from "anssi-nis2-ui/src/Domaine/Simulateur/Sectorisation.definitions";
-
-function extraitSousSecteurs(
-  secteur: SecteurActivite,
-  sousSecteurs: SousSecteurActivite[],
-): Sectorisation[] {
-  if (estUnSecteurSansSousSecteur(secteur) || sousSecteurs.length == 0)
-    return [{ type: "sansSousSecteur", secteur: secteur }];
-  return sousSecteurs
-    .filter(estDansSecteur(secteur as SecteursAvecSousSecteurs))
-    .reduce(
-      (acc, sousSecteur) => [
-        ...acc,
-        {
-          type: "avecSousSecteur",
-          secteur: secteur,
-          sousSecteur: sousSecteur,
-        },
-      ],
-      [],
-    );
-}
-
-function extraitCouplesSectoriels(
-  reponses: IDonneesBrutesFormulaireSimulateur,
-): Sectorisation[] {
-  return reponses.secteurActivite.reduce(
-    (acc, secteur) => [
-      ...acc,
-      ...extraitSousSecteurs(secteur, reponses.sousSecteurActivite),
-    ],
-    [],
-  );
-}
 
 @Injectable()
 export class JournalService {
@@ -76,7 +36,7 @@ export class JournalService {
     };
     // return [evenement];
     const resultat: SegmentsConcernesNis2[] = [];
-    const valeursSectorielles = extraitCouplesSectoriels(reponses);
+    const valeursSectorielles = extraitSectorisationDonneesSimulateur(reponses);
     for (const sectorisation of valeursSectorielles) {
       const { type, secteur } = sectorisation;
       const sousSecteur =
---
+++ anssi-nis2-ui/src/Domaine/Simulateur/services/Sectorisation/Sectorisation.operations.ts
@@ -0,0 +1,30 @@
+import { IDonneesBrutesFormulaireSimulateur } from "../../DonneesFormulaire";
+import { Sectorisation } from "../../Sectorisation.definitions";
+import { SousSecteurActivite } from "../../SousSecteurActivite.definitions";
+import { SecteurActivite } from "../../SecteurActivite.definitions";
+import { estUnSecteurSansSousSecteur } from "../SecteurActivite/SecteurActivite.predicats";
+import { estDansSecteur } from "../SousSecteurActivite/SousSecteurActivite.predicats";
+
+const extraitSectorisationPourSecteur =
+  (sousSecteurs: SousSecteurActivite[]) =>
+  (secteur: SecteurActivite): Sectorisation[] => {
+    if (estUnSecteurSansSousSecteur(secteur) || sousSecteurs.length == 0)
+      return [{ type: "sansSousSecteur", secteur: secteur }];
+    return sousSecteurs.filter(estDansSecteur(secteur)).reduce(
+      (acc, sousSecteur) => [
+        ...acc,
+        {
+          type: "avecSousSecteur",
+          secteur: secteur,
+          sousSecteur: sousSecteur,
+        },
+      ],
+      [] as Sectorisation[],
+    );
+  };
+export const extraitSectorisationDonneesSimulateur = (
+  reponses: IDonneesBrutesFormulaireSimulateur,
+): Sectorisation[] =>
+  reponses.secteurActivite.flatMap(
+    extraitSectorisationPourSecteur(reponses.sousSecteurActivite),
+  );
