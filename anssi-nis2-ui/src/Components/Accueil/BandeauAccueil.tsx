fe1cc89127fc5f31fc4f979dbe1c252cebd8875a
GalloIT <bgallay@palo-it.com>
2023-12-19 09:28:45 +0100

    feat: ajuste la largeur "Suis-je concerné"

--- anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
@@ -1,29 +1,27 @@
 import { DefaultComponent } from "../../Services/Props";
 import { RowContainer } from "../General/RowContainer.tsx";
 
-const BandeauAccueil: DefaultComponent = () => {
-  return (
-    <div className="fr-pt-5w fr-bandeau-marianne fr-nis2-accueil-principal">
-      <div className="fr-my-0">
-        <RowContainer>
-          <div className="fr-container">
-            <div className="fr-grid-row fr-pt-12w">
-              <div className="fr-col-offset-0 fr-nis2-appel-action fr-mb-8w">
-                <div>
-                  <h1>MonEspaceNIS2</h1>
-                  <p>
-                    Accompagner les organisations dans la compréhension
-                    <br />
-                    et la mise en conformité à NIS&nbsp;2
-                  </p>
-                </div>
+const BandeauAccueil: DefaultComponent = () => (
+  <div className="fr-pt-5w fr-bandeau-marianne fr-nis2-accueil-principal">
+    <div className="fr-my-0">
+      <RowContainer>
+        <div className="fr-container">
+          <div className="fr-grid-row fr-pt-12w">
+            <div className="fr-col-offset-0 fr-nis2-appel-action fr-mb-8w">
+              <div>
+                <h1>MonEspaceNIS2</h1>
+                <p>
+                  Accompagner les organisations dans la compréhension
+                  <br />
+                  et la mise en conformité à NIS&nbsp;2
+                </p>
               </div>
             </div>
           </div>
-        </RowContainer>
-      </div>
+        </div>
+      </RowContainer>
     </div>
-  );
-};
+  </div>
+);
 
 export default BandeauAccueil;
--- anssi-nis2-ui/src/Components/Accueil/BandeauConcerne.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauConcerne.tsx
@@ -3,25 +3,27 @@ import { noRefClick } from "../../Services/Echaffaudages/AssistantsEchaffaudages
 import { Link } from "react-router-dom";
 import { DefaultComponent } from "../../Services/Props";
 
-const BandeauConcerne: DefaultComponent = () => {
-  return (
-    <>
-      <div className="fr-nis2-suis-je-concerne fr-pt-10w fr-pb-13w">
-        <div className="fr-nis2-flottant">
-          <h2 className="fr-h1">Suis-je concerné ?</h2>
-          <p className="fr-text--lead">
-            Découvrez en 2 minutes si la directive NIS 2 peut s’appliquer à
-            votre entité.
-          </p>
-          <Link to={"/simulateur"}>
-            <Button className="fr-btn" onClick={noRefClick}>
-              Débuter le test
-            </Button>
-          </Link>
+const BandeauConcerne: DefaultComponent = () => (
+  <>
+    <div className="fr-nis2-suis-je-concerne fr-pt-10w fr-pb-13w">
+      <div className="fr-container">
+        <div className="fr-grid-row">
+          <div className="fr-nis2-flottant fr-col-offset-0 fr-col-12">
+            <h2 className="fr-h1">Suis-je concerné ?</h2>
+            <p className="fr-text--lead">
+              Découvrez en 2 minutes si la directive NIS 2 peut s&apos;appliquer
+              à votre entité.
+            </p>
+            <Link to={"/simulateur"}>
+              <Button className="fr-btn" onClick={noRefClick}>
+                Débuter le test
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 
 export default BandeauConcerne;
