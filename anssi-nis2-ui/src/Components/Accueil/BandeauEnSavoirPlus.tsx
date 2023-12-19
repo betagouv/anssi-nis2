5969d9b8858cfd2caf3476ada300fd55df650f31
GalloIT <bgallay@palo-it.com>
2023-12-19 14:31:34 +0100

    fix: étend le fond bleu clair "En savoir plus" de la page d'accueil

--- anssi-nis2-ui/src/Components/Accueil/BandeauEnSavoirPlus.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauEnSavoirPlus.tsx
@@ -8,32 +8,34 @@ import imageFAQ from "../../assets/faq.svg";
 const BandeauEnSavoirPlus: DefaultComponent = () => {
   return (
     <>
-      <div className="fr-container fr-nis2-bandeau-marianne-leger fr-nis2-bordure-bas-l">
-        <div className="fr-py-7w">
-          <h2 className="fr-h1 fr-mb-5w">En savoir plus</h2>
-          <div className="fr-grid-row--center">
-            <div className="fr-col-8 fr-col-offset-2 ">
-              <p className="fr-text--lead fr-mb-5w">
-                Retrouver ci-dessous les éléments de contexte pour comprendre
-                plus en détail les enjeux et modalités de la directive.
-              </p>
-              <ul className="fr-nis2-tuile-lien fr-grid-row">
-                <TuileLien
-                  image={imageSourceUE}
-                  titre={"Directive NIS 2"}
-                  lien={liens.legislation.europa}
-                />
-                <TuileLien
-                  image={imageWebinaire}
-                  titre={"Présentation vidéo"}
-                  lien={liens.anssi.webinaire}
-                />
-                <TuileLien
-                  image={imageFAQ}
-                  titre={"FAQ"}
-                  lien={liens.anssi.faq}
-                />
-              </ul>
+      <div className="fr-nis2-bandeau-marianne-leger">
+        <div className="fr-container fr-nis2-bordure-bas-l">
+          <div className="fr-py-7w">
+            <h2 className="fr-h1 fr-mb-5w">En savoir plus</h2>
+            <div className="fr-grid-row--center">
+              <div className="fr-col-8 fr-col-offset-2 ">
+                <p className="fr-text--lead fr-mb-5w">
+                  Retrouver ci-dessous les éléments de contexte pour comprendre
+                  plus en détail les enjeux et modalités de la directive.
+                </p>
+                <ul className="fr-nis2-tuile-lien fr-grid-row">
+                  <TuileLien
+                    image={imageSourceUE}
+                    titre={"Directive NIS 2"}
+                    lien={liens.legislation.europa}
+                  />
+                  <TuileLien
+                    image={imageWebinaire}
+                    titre={"Présentation vidéo"}
+                    lien={liens.anssi.webinaire}
+                  />
+                  <TuileLien
+                    image={imageFAQ}
+                    titre={"FAQ"}
+                    lien={liens.anssi.faq}
+                  />
+                </ul>
+              </div>
             </div>
           </div>
         </div>
