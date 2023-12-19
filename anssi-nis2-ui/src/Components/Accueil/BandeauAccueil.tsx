7035069a27f43b39531d6cb8a18890e3316cac0a
GalloIT <bgallay@palo-it.com>
2023-12-19 10:39:44 +0100

    fix: ajuste les tailles de police et positions des 2 premiers blocs sur page d'accueil

--- anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
@@ -9,8 +9,8 @@ const BandeauAccueil: DefaultComponent = () => (
         <div className="fr-container fr-pl-0">
           <div className="fr-grid-row ">
             <div className="fr-col-offset-0 fr-col-7 fr-nis2-appel-action">
-              <div className="fr-pt-12w">
-                <h1>MonEspaceNIS2</h1>
+              <div className="fr-pt-32v">
+                <h1 className="fr-pt-3w">MonEspaceNIS2</h1>
                 <p>
                   Accompagner les organisations dans la compréhension
                   <br />
@@ -18,7 +18,7 @@ const BandeauAccueil: DefaultComponent = () => (
                 </p>
               </div>
             </div>
-            <div className="fr-col-5 fr-nis2-marches">
+            <div className="fr-col-5">
               <img src={imageSrcMarches} alt="Personne montant les marches" />
             </div>
           </div>
