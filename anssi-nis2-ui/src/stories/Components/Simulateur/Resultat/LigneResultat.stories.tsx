9429c649ab9cbcee0244f4e4034c1dad7b36958a
GalloIT <bgallay@palo-it.com>
2024-01-08 10:21:06 +0100

    feat: affiche en 2 partie le résultat Grande Entreprise et le résultat négatif

--- anssi-nis2-ui/src/stories/Components/Simulateur/Resultat/LigneResultat.stories.tsx
+++ anssi-nis2-ui/src/stories/Components/Simulateur/Resultat/LigneResultat.stories.tsx
@@ -2,7 +2,11 @@ import { expect } from "@storybook/jest";
 import { Meta, StoryObj } from "@storybook/react";
 import { userEvent, within } from "@storybook/testing-library";
 import { LigneResultat } from "../../../../Components/Simulateur/Resultats/LigneResultat.tsx";
-import { contenusResultatEligiblePetitEntreprise } from "../../../../References/contenusResultatEligibilite.ts";
+import {
+  contenusResultatEligibleGrandeEntreprise,
+  contenusResultatEligiblePetitEntreprise,
+  contenusResultatNonEligible,
+} from "../../../../References/contenusResultatEligibilite.ts";
 
 const meta: Meta<typeof LigneResultat> = {
   title: "Composants/Simulateur/Résultat",
@@ -27,6 +31,50 @@ export const LigneResultatPetiteEntreprise: Story = {
     await expect(moinsInformations).toBeVisible();
     await userEvent.click(moinsInformations);
     await expect(await canvas.queryByText("REC")).not.toBeVisible();
+    await canvas.findByText("Plus d'informations");
+  },
+};
+
+export const LigneResultatMoyenneGrandeEntreprise: Story = {
+  args: {
+    contenuResultat: contenusResultatEligibleGrandeEntreprise,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    expect(await canvas.findByText("REC")).not.toBeVisible();
+
+    await userEvent.click(await canvas.findByText("Plus d'informations"));
+    await expect(await canvas.findByText("REC")).toBeVisible();
+    const moinsInformations = await canvas.findByText("Moins d'informations");
+    await expect(moinsInformations).toBeVisible();
+    await userEvent.click(moinsInformations);
+    await expect(await canvas.queryByText("REC")).not.toBeVisible();
+    await canvas.findByText("Plus d'informations");
+  },
+};
+
+export const LigneResultatNonEligible: Story = {
+  args: {
+    contenuResultat: contenusResultatNonEligible,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    expect(
+      await canvas.findByText("Critères de possible inclusion"),
+    ).not.toBeVisible();
+
+    await userEvent.click(await canvas.findByText("Plus d'informations"));
+    await expect(
+      await canvas.findByText("Critères de possible inclusion"),
+    ).toBeVisible();
+    const moinsInformations = await canvas.findByText("Moins d'informations");
+    await expect(moinsInformations).toBeVisible();
+    await userEvent.click(moinsInformations);
+    await expect(
+      await canvas.queryByText("Critères de possible inclusion"),
+    ).not.toBeVisible();
     await canvas.findByText("Plus d'informations");
   },
 };
