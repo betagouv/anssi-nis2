9a7977aa22b4f00d058b487c620a8478bc5fea82
GalloIT <bgallay@palo-it.com>
2024-01-17 18:45:45 +0100

    feat: extrait et transforme la front matter du doc et du titre

--- anssi-nis2-ui/src/Services/Markdown/TransformeMarkdown.operations.ts
+++ anssi-nis2-ui/src/Services/Markdown/TransformeMarkdown.operations.ts
@@ -2,19 +2,46 @@ export const nettoieBrMd = (texte: string) => texte.replace("  \n", " ");
 export const separeMarkdownParLignes = (texteMarkdown: string) =>
   `${texteMarkdown}`.split("---");
 
+const regexFrontMatterBase = /^\s*---\n([\s\S]*?)\n\s*---/;
+const regexFrontMatterSections = /---\n([\s\S]*?)\n\s*---\n\s*[#]+([^\n]+)\n/g;
 export const extraitFrontMatterBrute = (texte: string) => {
-  const res = texte.match(/---\n([\s\S]*?)\n\s*---/);
+  const res = texte.match(regexFrontMatterBase);
   return res ? res[1].trim() : "";
 };
+export const extraitFrontMatterSectionsBrute = (texte: string) =>
+  Array.from(texte.matchAll(regexFrontMatterSections)).reduce(
+    (acc: string[][], m) => [...acc, [m[1].trim(), m[2].trim()]],
+    [],
+  );
 
 const decoupeFrontMatterBrute = (texte: string) =>
-  texte
-    .split("\n")
-    .map((line) => line.split(":"))
-    .reduce(
-      (acc, couple) => ({ ...acc, [couple[0].trim()]: couple[1].trim() }),
-      {},
-    );
+  texte === ""
+    ? {}
+    : texte
+        .split("\n")
+        .map((line) => line.split(":"))
+        .reduce(
+          (acc, couple) => ({ ...acc, [couple[0].trim()]: couple[1].trim() }),
+          {},
+        );
 
-export const extraitFrontMatter = (texte: string) =>
-  texte === "" ? {} : decoupeFrontMatterBrute(extraitFrontMatterBrute(texte));
+const extraitSections = (texte: string) =>
+  extraitFrontMatterSectionsBrute(texte).length === 0
+    ? {}
+    : {
+        sections: extraitFrontMatterSectionsBrute(texte).reduce(
+          (acc: unknown[], elt) => [
+            ...acc,
+            {
+              titre: elt[1],
+              ...decoupeFrontMatterBrute(elt[0]),
+            },
+          ],
+          [],
+        ),
+      };
+
+export const extraitFrontMatter = (texte: string) => ({
+  ...decoupeFrontMatterBrute(extraitFrontMatterBrute(texte)),
+  ...extraitSections(texte),
+});
--- anssi-nis2-ui/test/Services/Markdown/extraitFrontMatter.spec.ts
+++ anssi-nis2-ui/test/Services/Markdown/extraitFrontMatter.spec.ts
@@ -2,7 +2,54 @@ import { describe, it, expect } from "vitest";
 import {
   extraitFrontMatter,
   extraitFrontMatterBrute,
+  extraitFrontMatterSectionsBrute,
 } from "../../../src/Services/Markdown/TransformeMarkdown.operations";
+
+describe(extraitFrontMatterSectionsBrute, () => {
+  it("retourne le contenu simple", () => {
+    const texteMarkdown = `
+---
+champ: contenu
+---
+# titre
+    `;
+    expect(extraitFrontMatterSectionsBrute(texteMarkdown)).toEqual([
+      ["champ: contenu", "titre"],
+    ]);
+  });
+  it("retourne le contenu simple", () => {
+    const texteMarkdown = `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        sed do eiusmod tempor incididunt ut labore
+        
+        ---
+        champA1: contenu A1
+        champA2: contenu A2
+        ---
+        ## Titre de section A
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        
+        ---
+        champB1: contenu B1
+        champB2: contenu B2
+        ---
+        ## Titre de section B
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        `;
+    expect(extraitFrontMatterSectionsBrute(texteMarkdown)).toEqual([
+      [
+        "champA1: contenu A1\n        champA2: contenu A2",
+        "Titre de section A",
+      ],
+      [
+        "champB1: contenu B1\n        champB2: contenu B2",
+        "Titre de section B",
+      ],
+    ]);
+  });
+});
 
 describe(extraitFrontMatterBrute, () => {
   it("retourne le contenu simple", () => {
@@ -24,29 +71,91 @@ champ: contenu
 });
 
 describe(extraitFrontMatter, () => {
-  it("n'extrait rien pour un Front Matter vide", () => {
-    expect(extraitFrontMatter("")).toStrictEqual({});
-  });
-  it("extrait pour un champ simple", () => {
-    const texteMarkdown = `
-    ---
-    champ: contenu
-    ---
-    `;
-    expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
-      champ: "contenu",
+  describe("seul dans le document", () => {
+    it("n'extrait rien pour un Front Matter vide", () => {
+      expect(extraitFrontMatter("")).toStrictEqual({});
+    });
+    it("extrait pour un champ simple", () => {
+      const texteMarkdown = `
+        ---
+        champ: contenu
+        ---
+        `;
+      expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
+        champ: "contenu",
+      });
+    });
+    it("extrait pour un champ simple", () => {
+      const texteMarkdown = `
+        ---
+        champ: contenu
+        champ2: contenu 2
+        ---
+        `;
+      expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
+        champ: "contenu",
+        champ2: "contenu 2",
+      });
     });
   });
-  it("extrait pour un champ simple", () => {
-    const texteMarkdown = `
-    ---
-    champ: contenu
-    champ2: contenu 2
-    ---
-    `;
-    expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
-      champ: "contenu",
-      champ2: "contenu 2",
+  describe("Au milieu d'un document, inclue le titre qui suit", () => {
+    it("front matter d'un titre", () => {
+      const texteMarkdown = `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        sed do eiusmod tempor incididunt ut labore
+        ---
+        champ: contenu
+        champ2: contenu 2
+        ---
+        ## Titre de section
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        
+        `;
+      expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
+        sections: [
+          {
+            titre: "Titre de section",
+            champ: "contenu",
+            champ2: "contenu 2",
+          },
+        ],
+      });
+    });
+    it("front matter de 2 titres", () => {
+      const texteMarkdown = `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        sed do eiusmod tempor incididunt ut labore
+        ---
+        champA1: contenu A1
+        champA2: contenu A2
+        ---
+        ## Titre de section A
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        
+        ---
+        champB1: contenu B1
+        champB2: contenu B2
+        ---
+        ## Titre de section B
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        `;
+      expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
+        sections: [
+          {
+            titre: "Titre de section A",
+            champA1: "contenu A1",
+            champA2: "contenu A2",
+          },
+          {
+            titre: "Titre de section B",
+            champB1: "contenu B1",
+            champB2: "contenu B2",
+          },
+        ],
+      });
     });
   });
 });
