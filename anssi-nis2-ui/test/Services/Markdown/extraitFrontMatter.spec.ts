08293c8cb8a939c893fe065d748a81dc08936e5f
GalloIT <bgallay@palo-it.com>
2024-01-18 09:35:07 +0100

    refacto: génère des markdown simples avec front matter

--- anssi-nis2-ui/test/Services/Markdown/extraitFrontMatter.spec.ts
+++ anssi-nis2-ui/test/Services/Markdown/extraitFrontMatter.spec.ts
@@ -1,72 +1,92 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import {
   extraitFrontMatter,
   extraitFrontMatterBrute,
   extraitFrontMatterSectionsBrute,
 } from "../../../src/Services/Markdown/TransformeMarkdown.operations";
 
+type ChampFrontMatter = [string, string];
+const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  sed do eiusmod tempor incididunt ut labore`;
+const champ = ([nom, contenu]: ChampFrontMatter) => `${nom}: ${contenu}`;
+const frontMatter = (champBrut: string) => `
+  ---
+  ${champBrut}
+  ---
+  `;
+const champs = (listeChamps: ChampFrontMatter[]) =>
+  listeChamps.map(champ).join("\n");
+
+const exempleFrontMatterDeuxChamps = frontMatter(
+  champs([
+    ["champ", "contenu"],
+    ["champ2", "contenu 2"],
+  ]),
+);
+const exempleFrontMatterAvecUnTitre = `
+  ${frontMatter(champs([["champ", "contenu"]]))}
+  # titre
+  `;
+const exMarkdownDeuxChapitresAvecFrontMatter = `
+  ${loremIpsum}
+  
+  ${frontMatter(
+    champs([
+      ["champA1", "contenu A1"],
+      ["champA2", "contenu A2"],
+    ]),
+  )}
+  ## Titre de section A
+  
+  ${loremIpsum}
+  
+  ${frontMatter(
+    champs([
+      ["champB1", "contenu B1"],
+      ["champB2", "contenu B2"],
+    ]),
+  )}
+  ## Titre de section B
+  
+  ${loremIpsum}
+  `;
+const exMarkdownUnChapitreAvecFrontMatter = `
+  ${loremIpsum}
+  ${frontMatter(
+    champs([
+      ["champ", "contenu"],
+      ["champ2", "contenu 2"],
+    ]),
+  )}
+  ## Titre de section
+  
+  ${loremIpsum}`;
 describe(extraitFrontMatterSectionsBrute, () => {
   it("retourne le contenu simple", () => {
-    const texteMarkdown = `
----
-champ: contenu
----
-# titre
-    `;
-    expect(extraitFrontMatterSectionsBrute(texteMarkdown)).toEqual([
-      ["champ: contenu", "titre"],
-    ]);
+    expect(
+      extraitFrontMatterSectionsBrute(exempleFrontMatterAvecUnTitre),
+    ).toEqual([["champ: contenu", "titre"]]);
   });
   it("retourne le contenu simple", () => {
-    const texteMarkdown = `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        sed do eiusmod tempor incididunt ut labore
-        
-        ---
-        champA1: contenu A1
-        champA2: contenu A2
-        ---
-        ## Titre de section A
-        
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        
-        ---
-        champB1: contenu B1
-        champB2: contenu B2
-        ---
-        ## Titre de section B
-        
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        `;
-    expect(extraitFrontMatterSectionsBrute(texteMarkdown)).toEqual([
-      [
-        "champA1: contenu A1\n        champA2: contenu A2",
-        "Titre de section A",
-      ],
-      [
-        "champB1: contenu B1\n        champB2: contenu B2",
-        "Titre de section B",
-      ],
+    expect(
+      extraitFrontMatterSectionsBrute(exMarkdownDeuxChapitresAvecFrontMatter),
+    ).toEqual([
+      ["champA1: contenu A1\nchampA2: contenu A2", "Titre de section A"],
+      ["champB1: contenu B1\nchampB2: contenu B2", "Titre de section B"],
     ]);
   });
 });
 
 describe(extraitFrontMatterBrute, () => {
   it("retourne le contenu simple", () => {
-    const texteMarkdown = `
----
-champ: contenu
----
-    `;
-    expect(extraitFrontMatterBrute(texteMarkdown)).toEqual("champ: contenu");
+    expect(
+      extraitFrontMatterBrute(frontMatter(champs([["champ", "contenu"]]))),
+    ).toEqual("champ: contenu");
   });
   it("retourne le contenu simple, insensible à l'indentation", () => {
-    const texteMarkdown = `
-    ---
-    champ: contenu
-    ---
-    `;
-    expect(extraitFrontMatterBrute(texteMarkdown)).toEqual("champ: contenu");
+    expect(
+      extraitFrontMatterBrute(frontMatter(champs([["champ", "contenu"]]))),
+    ).toEqual("champ: contenu");
   });
 });
 
@@ -76,23 +96,14 @@ describe(extraitFrontMatter, () => {
       expect(extraitFrontMatter("")).toStrictEqual({});
     });
     it("extrait pour un champ simple", () => {
-      const texteMarkdown = `
-        ---
-        champ: contenu
-        ---
-        `;
-      expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
+      expect(
+        extraitFrontMatter(frontMatter(champs([["champ", "contenu"]]))),
+      ).toStrictEqual({
         champ: "contenu",
       });
     });
     it("extrait pour un champ simple", () => {
-      const texteMarkdown = `
-        ---
-        champ: contenu
-        champ2: contenu 2
-        ---
-        `;
-      expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
+      expect(extraitFrontMatter(exempleFrontMatterDeuxChamps)).toStrictEqual({
         champ: "contenu",
         champ2: "contenu 2",
       });
@@ -100,19 +111,9 @@ describe(extraitFrontMatter, () => {
   });
   describe("Au milieu d'un document, inclue le titre qui suit", () => {
     it("front matter d'un titre", () => {
-      const texteMarkdown = `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        sed do eiusmod tempor incididunt ut labore
-        ---
-        champ: contenu
-        champ2: contenu 2
-        ---
-        ## Titre de section
-        
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        
-        `;
-      expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
+      expect(
+        extraitFrontMatter(exMarkdownUnChapitreAvecFrontMatter),
+      ).toStrictEqual({
         sections: [
           {
             titre: "Titre de section",
@@ -123,26 +124,9 @@ describe(extraitFrontMatter, () => {
       });
     });
     it("front matter de 2 titres", () => {
-      const texteMarkdown = `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        sed do eiusmod tempor incididunt ut labore
-        ---
-        champA1: contenu A1
-        champA2: contenu A2
-        ---
-        ## Titre de section A
-        
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        
-        ---
-        champB1: contenu B1
-        champB2: contenu B2
-        ---
-        ## Titre de section B
-        
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        `;
-      expect(extraitFrontMatter(texteMarkdown)).toStrictEqual({
+      expect(
+        extraitFrontMatter(exMarkdownDeuxChapitresAvecFrontMatter),
+      ).toStrictEqual({
         sections: [
           {
             titre: "Titre de section A",
