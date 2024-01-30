3a277b003ded3a9b7f5d390683668156cd857763
GalloIT <bgallay@palo-it.com>
2024-01-30 12:50:04 +0100

    fix: decore la page faq pour l'affichage SB

--- anssi-nis2-ui/src/stories/Components/Transverses/PageFAQ.stories.tsx
+++ anssi-nis2-ui/src/stories/Components/Transverses/PageFAQ.stories.tsx
@@ -1,9 +1,11 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { PageFaq } from "../../../Components/PageFaq.tsx";
+import { pageDecorator } from "../../utilitaires/PageDecorator.tsx";
 
 const meta: Meta<typeof PageFaq> = {
   title: "Composants/Transverses/FAQ",
   component: PageFaq,
+  decorators: [pageDecorator],
 };
 
 export default meta;
