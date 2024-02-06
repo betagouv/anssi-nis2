ecc0e9204c87e28bedd1eafee96d497456bc13cd
GalloIT <bgallay@palo-it.com>
2024-02-06 17:19:19 +0100

    refacto: nettoie commentaires

--- anssi-nis2-api/src/main.ts
+++ anssi-nis2-api/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as process from "process";
 import { ValidationPipe } from "@nestjs/common";
-// import { SentryService } from "@ntegral/nestjs-sentry";
 
 const listeningPort = process.env.PORT || 3000;
 
@@ -10,7 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix("api");
-  // app.useLogger(SentryService.SentryServiceInstance());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(listeningPort);
 }
