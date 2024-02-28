df457335fb112b9d04152b3b377dbccc491acf86
GalloIT <bgallay@palo-it.com>
2024-02-28 11:01:58 +0100

    fix: rectifie le positionnement du texte par rapport aux marches

--- anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
@@ -1,6 +1,6 @@
 import { DefaultComponent } from "../../Services/Props";
 import { RowContainer } from "../General/RowContainer.tsx";
-import imageSrcMarches from "../../assets/marches.svg";
+// import imageSrcMarches from "../../assets/marches.svg";
 
 const BandeauAccueil: DefaultComponent = () => (
   <div className="fr-bandeau-marianne fr-nis2-accueil-principal">
@@ -8,7 +8,7 @@ const BandeauAccueil: DefaultComponent = () => (
       <RowContainer>
         <div className="fr-container fr-pl-0">
           <div className="fr-grid-row ">
-            <div className="fr-col-offset-0 fr-col-7 fr-nis2-appel-action">
+            <div className="fr-col-offset-0 fr-col-8 fr-nis2-appel-action">
               <div className="fr-pt-32v">
                 <h1 className="fr-pt-3w">MonEspaceNIS2</h1>
                 <p>
@@ -18,9 +18,6 @@ const BandeauAccueil: DefaultComponent = () => (
                   et les accompagner dans leur mise en conformité
                 </p>
               </div>
-            </div>
-            <div className="fr-col-5">
-              <img src={imageSrcMarches} alt="Personne montant les marches" />
             </div>
           </div>
         </div>
