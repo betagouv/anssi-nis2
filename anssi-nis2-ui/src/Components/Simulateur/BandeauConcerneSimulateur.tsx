ddc08946716b19b3db2f231178e7004d85c51d16
GalloIT <bgallay@palo-it.com>
2024-03-05 17:33:52 +0100

    feat: change le texte "Mon organisation est-elle concernée ?"

--- anssi-nis2-ui/src/Components/Simulateur/BandeauConcerneSimulateur.tsx
+++ anssi-nis2-ui/src/Components/Simulateur/BandeauConcerneSimulateur.tsx
@@ -4,9 +4,10 @@ import { BandeauBleuMarianne } from "../General/BandeauBleuMarianne.tsx";
 export const BandeauConcerneSimulateur: DefaultComponent = () => (
   <>
     <BandeauBleuMarianne>
-      <h2 className="fr-mb-1w">Suis-je concerné ?</h2>
+      <h2 className="fr-mb-1w">Mon organisation est-elle concernée ?</h2>
       <p className="fr-mb-0">
-        Découvrez si la directive NIS&nbsp;2 s’appliquera à votre entité
+        Réalisez un test pour déterminer si votre organisation est assujettie à
+        la directive NIS&nbsp;2
       </p>
     </BandeauBleuMarianne>
   </>
