1327286e7dd92b7a707188a80f5b04bd88ffec25
GalloIT <bgallay@palo-it.com>
2024-03-11 11:35:45 +0100

    feat: change le texte du bandeau suis-je concerné

--- anssi-nis2-ui/src/Components/Simulateur/BandeauConcerneSimulateur.tsx
+++ anssi-nis2-ui/src/Components/Simulateur/BandeauConcerneSimulateur.tsx
@@ -1,13 +1,12 @@
+import { libellesAccueil } from "../../References/LibellesAccueil.ts";
 import { DefaultComponent } from "../../Services/Props";
 import { BandeauBleuMarianne } from "../General/BandeauBleuMarianne.tsx";
 
 export const BandeauConcerneSimulateur: DefaultComponent = () => (
   <>
     <BandeauBleuMarianne>
-      <h2 className="fr-mb-1w">Suis-je concerné ?</h2>
-      <p className="fr-mb-0">
-        Découvrez si la directive NIS&nbsp;2 s’appliquera à votre entité
-      </p>
+      <h2 className="fr-mb-1w">{libellesAccueil.concerne.titre}</h2>
+      <p className="fr-mb-0">{libellesAccueil.concerne.resume}</p>
     </BandeauBleuMarianne>
   </>
 );
