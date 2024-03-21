644a1ccb2536dd9b61b1531a0b2a9f2b91df4e98
Christophe Cadilhac <christophe@malea.dev>
2024-03-21 16:54:25 +0100

    feat: utilise l'option `trust proxy`
    ... pour récupérer dans `req.ip` l'IP de l'utilisateur.
    Sans ça, l'IP utilisée est celle du reverse proxy. Cela compromet l'utilisation des Throttlers et filtrage IP.

--- anssi-nis2-api/src/main.ts
+++ anssi-nis2-api/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as process from "process";
 import { ValidationPipe } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 const listeningPort = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
   app.enableCors();
   app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
