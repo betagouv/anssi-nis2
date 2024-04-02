f3917d711afed7f22d975f999648e4f247b6ccb6
GalloIT <bgallay@palo-it.com>
2024-04-02 10:43:10 +0200

    refacto: supprime les FAQ inutiles

--- anssi-nis2-ui/src/Services/Markdown/TransformeMarkdown.operations.ts
+++ anssi-nis2-ui/src/Services/Markdown/TransformeMarkdown.operations.ts
@@ -1,14 +1,4 @@
 import { replace, split } from "fp-ts/lib/string";
-import { remark } from "remark";
-import remarkFrontmatter from "remark-frontmatter";
-import { extraitMatiere } from "./ExtraitMatiere.plugin.ts";
-import { StructureMarkdown } from "./Markdown.declarations.ts";
 
 export const nettoieBrMd = replace("  \n", " ");
 export const separeMarkdownParLignes = split("---");
-
-export const extraitFrontMatter = (texte: string): StructureMarkdown =>
-  remark()
-    .use(remarkFrontmatter, { marker: "-", type: "yaml", anywhere: true })
-    .use(extraitMatiere)
-    .processSync(texte).data.frontmatter as StructureMarkdown;
