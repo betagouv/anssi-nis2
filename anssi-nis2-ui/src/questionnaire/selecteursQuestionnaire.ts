b1c042ead61c8afa54e763996f8b35434217489e
GalloIT <bgallay@palo-it.com>
2024-04-04 15:54:07 +0200

    refacto: utilise une fonction générique

--- anssi-nis2-ui/src/questionnaire/selecteursQuestionnaire.ts
+++ anssi-nis2-ui/src/questionnaire/selecteursQuestionnaire.ts
@@ -1,5 +1,6 @@
 import { SecteurSimple } from "anssi-nis2-core/src/Domain/Simulateur/SecteurActivite.definitions.ts";
 import { SousSecteurActivite } from "anssi-nis2-core/src/Domain/Simulateur/SousSecteurActivite.definitions.ts";
+import { estSousSecteurListe } from "../../../commun/core/src/Domain/Simulateur/services/SousSecteurActivite/SousSecteurActivite.predicats.ts";
 import { non } from "../../../commun/utils/services/commun.predicats.ts";
 import { estUnSecteurAvecDesSousSecteurs } from "anssi-nis2-core/src/Domain/Simulateur/services/SecteurActivite/SecteurActivite.predicats.ts";
 import { EtatQuestionnaire } from "./reducerQuestionnaire.ts";
@@ -10,7 +11,7 @@ export const selectSecteursPourSaisieActivites = (
   const elements = [...etat.secteurActivite, ...etat.sousSecteurActivite];
   const sansSousSecteurs = elements
     .filter(non(estUnSecteurAvecDesSousSecteurs))
-    .filter((s) => !s.startsWith("autre"));
+    .filter(estSousSecteurListe);
 
   return sansSousSecteurs as (SecteurSimple | SousSecteurActivite)[];
 };
