4f0c09b2c214a04345be1fbf28df9fa68a015f6e
GalloIT <bgallay@palo-it.com>
2024-04-04 17:17:01 +0200

    refacto: supprime le strict mode

--- anssi-nis2-ui/src/app.tsx
+++ anssi-nis2-ui/src/app.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { AppContext } from "./Services/AppContexte/AppContext.definition.ts";
 import { defaultContext } from "./Services/AppContexte/AppContext.constantes.ts";
 import { router } from "./router.tsx";
@@ -6,10 +5,8 @@ import { RouterProvider } from "react-router-dom";
 
 export const App = () => {
   return (
-    <React.StrictMode>
-      <AppContext.Provider value={defaultContext}>
-        <RouterProvider router={router} />
-      </AppContext.Provider>
-    </React.StrictMode>
+    <AppContext.Provider value={defaultContext}>
+      <RouterProvider router={router} />
+    </AppContext.Provider>
   );
 };
