cc2199ce5a28322574396b562c23ea02266cf6e1
Christophe Cadilhac <christophe@malea.dev>
2024-04-06 00:09:09 +0200

    Supprime du code mort

--- anssi-nis2-ui/test/Services/Simulateur/simulateurFrontServices.spec.ts
+++ anssi-nis2-ui/test/Services/Simulateur/simulateurFrontServices.spec.ts
@@ -1,65 +1,11 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import { Activite } from "../../../../commun/core/src/Domain/Simulateur/Activite.definitions";
 import { donneesFormulaireSimulateurVide } from "../../../../commun/core/src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.constantes";
 import { fabriqueDonneesFormulaire } from "../../../../commun/core/src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.fabrique";
 import { genereTransformateurValeursVersOptions } from "../../../src/Services/Simulateur/genereTransformateurValeursVersOptions";
-import { libellesPaysUnionEuropeenneLocalisation } from "../../../src/References/Libelles";
-import { transformePaysUnionEuropeennePourSelect } from "../../../src/Services/Simulateur/Transformateurs/TransformePaysUnionEuropeennePourSelect";
 
 describe(genereTransformateurValeursVersOptions, () => {
   const onChange: React.ChangeEventHandler<HTMLInputElement> = () => {};
-  const optionsPaysUE = (optionsChecked = [false, false, false]) => [
-    {
-      label: "France",
-      nativeInputProps: {
-        checked: optionsChecked[0],
-        onChange: onChange,
-        name: "appartenancePaysUnionEuropeenne",
-        value: "france",
-      },
-    },
-    {
-      label: "Autres états membres de l'Union Européenne",
-      nativeInputProps: {
-        checked: optionsChecked[1],
-        onChange: onChange,
-        name: "appartenancePaysUnionEuropeenne",
-        value: "autre",
-      },
-    },
-    {
-      label: "Autres états hors Union Européenne",
-      nativeInputProps: {
-        checked: optionsChecked[2],
-        onChange: onChange,
-        name: "appartenancePaysUnionEuropeenne",
-        value: "horsue",
-      },
-    },
-  ];
-
-  it("transforme la liste de pays de l'UE par défaut", () => {
-    const attendu = optionsPaysUE();
-    expect(
-      transformePaysUnionEuropeennePourSelect(
-        libellesPaysUnionEuropeenneLocalisation,
-        onChange,
-      ),
-    ).toStrictEqual(attendu);
-  });
-
-  it("génère un champ d'option avec les bons états checked", () => {
-    const attendu = optionsPaysUE([true, false, false]);
-    const defaultDataForm = fabriqueDonneesFormulaire({
-      appartenancePaysUnionEuropeenne: ["france"],
-    });
-    const optionsPaysUEObtenu = transformePaysUnionEuropeennePourSelect(
-      libellesPaysUnionEuropeenneLocalisation,
-      onChange,
-      defaultDataForm,
-    );
-    expect(optionsPaysUEObtenu).toStrictEqual(attendu);
-  });
 
   type ValeurActivitesPartielles = Extract<
     Activite,
