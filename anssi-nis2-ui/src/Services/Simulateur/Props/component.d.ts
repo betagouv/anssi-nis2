51312eaa9ea7fe25f7f0b1a2479cf4d5817b12d6
Christophe Cadilhac <christophe@malea.dev>
2024-04-11 22:40:47 +0200

    Supprime du code mort

--- anssi-nis2-ui/src/Services/Simulateur/Props/component.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultComponentExtensible } from "../../Props";
-
-import { SimulateurEtapeRenderedProps } from "./simulateurEtapeProps";
-
-export type SimulateurEtapeRenderedComponent =
-  DefaultComponentExtensible<SimulateurEtapeRenderedProps>;
