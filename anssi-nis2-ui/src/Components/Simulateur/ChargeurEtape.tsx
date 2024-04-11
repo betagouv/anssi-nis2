dcf0549ac83be9f56b1782f7f5a9edb5bccabe77
Christophe Cadilhac <christophe@malea.dev>
2024-04-11 22:23:40 +0200

    Supprime le tracking du questionnaire par Matomo
    … qui ne sera utilisé par personne.
    
    Ça tire une pelote de suppression :
      - `etatEtapesInitial` devient du code mort
      - donc tout `EtapesQuestionnaire.ts` aussi
      - `useReducteurDonneesFormulaireDuContexte()` devient du code mort

--- anssi-nis2-ui/src/Components/Simulateur/ChargeurEtape.tsx
+++ anssi-nis2-ui/src/Components/Simulateur/ChargeurEtape.tsx
@@ -1,11 +1,6 @@
-import React, { useContext, useEffect, useReducer, useState } from "react";
+import React, { useContext, useReducer } from "react";
 import { Helmet } from "react-helmet";
-import { donneesFormulaireSimulateurVide } from "../../../../commun/core/src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.constantes.ts";
-
 import { DefaultComponent } from "../../Services/Props";
-import { etatEtapesInitial } from "./Etapes/EtapesQuestionnaire.ts";
-import { useReducteurDonneesFormulaireDuContexte } from "../../Services/AppContexte/AppContext.operations.ts";
-import { traceEtapeSimulateur } from "../../Services/TraceurWeb/traceEtapeSimulateur.ts";
 import { Questionnaire } from "./Questionnaire.tsx";
 import { AppContext } from "../../Services/AppContexte/AppContext.definition.ts";
 import { quiSupporteUndo } from "../../questionnaire/quiSupporteUndo.ts";
@@ -18,20 +13,9 @@ import { TitresEtapes } from "./TitresEtapes.ts";
 const ChargeurEtapeCalcule: DefaultComponent = () => {
   const { envoieDonneesFormulaire } = useContext(AppContext);
 
-  const [donneesFormulaireSimulateur] = useReducer(
-    useReducteurDonneesFormulaireDuContexte(),
-    donneesFormulaireSimulateurVide,
-  );
-  const [etatEtapes] = useState(etatEtapesInitial);
-
   const [etatQuestionnaire, dispatchQuestionnaire] = useReducer(
     quiSupporteUndo(reducerQuestionnaire, etatParDefaut),
     { courant: etatParDefaut, precedents: [] },
-  );
-
-  useEffect(
-    () => traceEtapeSimulateur(etatEtapes, donneesFormulaireSimulateur),
-    [donneesFormulaireSimulateur, etatEtapes],
   );
 
   return (
@@ -43,12 +27,11 @@ const ChargeurEtapeCalcule: DefaultComponent = () => {
         </title>
       </Helmet>
       <div id="debutForm"></div>
-
-        <Questionnaire
-          etat={etatQuestionnaire.courant}
-          dispatch={dispatchQuestionnaire}
-          envoieDonneesFormulaire={envoieDonneesFormulaire}
-        />
+      <Questionnaire
+        etat={etatQuestionnaire.courant}
+        dispatch={dispatchQuestionnaire}
+        envoieDonneesFormulaire={envoieDonneesFormulaire}
+      />
       )
     </>
   );
--- anssi-nis2-ui/src/Services/AppContexte/AppContext.operations.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { AppContext } from "./AppContext.definition.ts";
-
-export const useReducteurDonneesFormulaireDuContexte = () => {
-  const {
-    simulateur: { reducteurDonneesFormulaire },
-  } = useContext(AppContext);
-  return reducteurDonneesFormulaire;
-};
