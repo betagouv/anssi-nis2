9bfe3f17cbe45336b94a38c991546eca39590445
Christophe Cadilhac <christophe@malea.dev>
2024-04-11 23:07:45 +0200

    Supprime du code mort

--- anssi-nis2-ui/src/Services/Simulateur/Props/gestionValeursFormulaire.d.ts
+++
@@ -1,7 +0,0 @@
-import { IDonneesBrutesFormulaireSimulateur } from "../../../Domaine/Simulateur/DonneesFormulaire.definitions.ts";
-import { ValeurChampSimulateur } from "../../../Domaine/Simulateur/ChampsSimulateur.valeurs.ts";
-
-export type GestionValeursFormulaire = (
-  value: ValeurChampSimulateur,
-  donneesFormulaire: IDonneesBrutesFormulaireSimulateur,
-) => ValeurChampSimulateur[];
