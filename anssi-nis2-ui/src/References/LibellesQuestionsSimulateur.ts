931b9d1fcd84388bd85d41712f46f4a802641d92
Christophe Cadilhac <christophe@malea.dev>
2024-04-11 22:41:54 +0200

    Supprime du code mort

--- anssi-nis2-ui/src/References/LibellesQuestionsSimulateur.ts
+++ anssi-nis2-ui/src/References/LibellesQuestionsSimulateur.ts
@@ -1,14 +1,3 @@
-export const texteQuestionLocalisastionsServices =
-  "Dans quels états membres de l’Union Européenne fournissez-vous des services numériques\xa0?";
-export const texteQuestionLocalisastionsServicesMention =
-  "Fournisseur de réseaux de communications électroniques publics, ou" +
-  " Fournisseur de services de communications électroniques accessibles au public.";
-
-export const texteEtapeTaillePublique =
-  "Quelles sont les caractéristiques clés de votre organisation\xa0?";
-export const texteQuestionTaillePublique =
-  "Nombre d’employés (équivalents temps pleins)";
-
 export const texteInfobulleBoutonFermer = "Masquer le message";
 export const texteInfobulleIconeInformation =
   "Informations à propos de l'activité";
