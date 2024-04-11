fc0fddd501c0834eb9cff784df98e8677d0d5a36
GalloIT <bgallay@palo-it.com>
2024-04-11 18:26:38 +0200

    WIP: les futurs cas de tests (facultatifs car tous doivent être passants)

--- anssi-nis2-ui/test/questionnaire/evalueEtatRegulation.spec.ts
+++ anssi-nis2-ui/test/questionnaire/evalueEtatRegulation.spec.ts
@@ -45,4 +45,73 @@ describe("L'évaluation de l'état de régulation", () => {
     expect(etatRegulation.decision).toBe("Regule");
     expect(etatRegulation.typeEntite).toBe("EntiteEssentielle");
   });
+
+  it("priorise « Régulé EE » sur « Régulé EI » pour une ME (cas des 2 questions combinées sur les localisations)", () => {
+    const questionnaire = executer([
+      valideEtapeDesignation(["non"]),
+      valideEtapeAppartenanceUE(["france"]),
+      valideTypeStructure(["privee"]),
+      valideTailleEntitePrivee(["moyen"], ["petit"]),
+      valideSecteursActivite(["infrastructureNumerique"]),
+      valideActivites([
+        "fournisseurReseauxCommunicationElectroniquesPublics",
+        "fournisseurServicesInformatiqueNuage",
+      ]),
+      valideLocalisationEtablissementPrincipal(["france"], [], []),
+      valideLocalisationServicesNumeriques(["france"]),
+    ]);
+
+    const donneesReponse =
+      ConvertisseurDonneesBrutesVersEtatDonneesSimulateur.depuisDonneesFormulaireSimulateur(
+        questionnaire,
+      ) as EtatRegulation;
+
+    const etatRegulation = evalueEtatRegulation(
+      donneesReponse,
+    ) as EtatRegulationDefinitivement<"Regule">;
+
+    expect(etatRegulation.decision).toBe("Regule");
+    expect(etatRegulation.typeEntite).toBe("EntiteEssentielle");
+  });
+  it("priorise « Régulé EE » sur « Régulé EI » pour une GE (cas des 2 questions combinées sur les localisations)", () => {
+    const questionnaire = executer([
+      valideEtapeDesignation(["non"]),
+      valideEtapeAppartenanceUE(["france"]),
+      valideTypeStructure(["privee"]),
+      valideTailleEntitePrivee(["moyen"], ["grand"]),
+      valideSecteursActivite(["infrastructureNumerique"]),
+      valideActivites([
+        "fournisseurReseauxCommunicationElectroniquesPublics",
+        "fournisseurServicesInformatiqueNuage",
+      ]),
+      valideLocalisationEtablissementPrincipal(["france"], [], []),
+      valideLocalisationServicesNumeriques(["france"]),
+    ]);
+
+    const donneesReponse =
+      ConvertisseurDonneesBrutesVersEtatDonneesSimulateur.depuisDonneesFormulaireSimulateur(
+        questionnaire,
+      ) as EtatRegulation;
+
+    const etatRegulation = evalueEtatRegulation(
+      donneesReponse,
+    ) as EtatRegulationDefinitivement<"Regule">;
+
+    expect(etatRegulation.decision).toBe("Regule");
+    expect(etatRegulation.typeEntite).toBe("EntiteEssentielle");
+  });
+
+  it.todo(
+    "priorise « Régulé EE » sur « Régulé mais voir EM » (cas des 2 questions combinées sur les localisations)",
+  );
+  it.todo(
+    "priorise « Régulé EE » sur « Non régulé, hors UE » (cas des 2 questions combinées sur les localisations)",
+  );
+
+  it.todo(
+    "priorise « Régulé EI » sur « Régulé mais voir EM » (cas des 2 questions combinées sur les localisations)",
+  );
+  it.todo(
+    "priorise « Régulé EI » sur « Non régulé, hors UE » (cas des 2 questions combinées sur les localisations)",
+  );
 });
