1a3d9b4f34c6d02ead4ca25d220623e07ab0b509
Christophe Cadilhac <christophe@malea.dev>
2024-04-12 14:23:00 +0200

    Supprime le timeout d'1 seconde
    … qui n'a pas vraiment d'utilité.
    En PROD on a pas mal d'erreurs qui remontent vers Sentry à cause de ce timeout qui semble trop restrictif.

--- anssi-nis2-ui/src/Services/prepare.ts
+++ anssi-nis2-ui/src/Services/prepare.ts
@@ -2,14 +2,11 @@ import axios from "axios";
 
 const baseUrlApi = import.meta.env.NIS2_API_BASE_URL || "";
 
-const urlApi = baseUrlApi + "api";
+const urlApi = `${baseUrlApi}api`;
 
 type RoutesApi = "simulateur-reponse" | "informations-emails";
+
 export const genereClientApi = (route: RoutesApi = "simulateur-reponse") => {
   const url = `${urlApi}/${route}`;
-  console.log(`${genereClientApi.name} Using URL : ${url}`);
-  return axios.create({
-    baseURL: url,
-    timeout: 1000,
-  });
+  return axios.create({ baseURL: url });
 };
