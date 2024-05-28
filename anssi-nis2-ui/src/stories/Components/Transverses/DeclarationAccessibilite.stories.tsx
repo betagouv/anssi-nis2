be638f24f3fc2a66c41b6c950fc05b9e56b5bb66
Christophe Cadilhac <christophe@malea.dev>
2024-05-28 16:16:36 +0200

    Crée la page « Déclaration d'accessibilité »

---
+++ anssi-nis2-ui/src/stories/Components/Transverses/DeclarationAccessibilite.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta } from "@storybook/react";
+import { DeclarationAccessibilite } from "../../../Components/PagesEdito/DeclarationAccessibilite.tsx";
+
+const Index: Meta<typeof DeclarationAccessibilite> = {
+  title: "Composants/Transverses/Déclaration d'accessibilité",
+  component: DeclarationAccessibilite,
+  parameters: {},
+};
+
+export default Index;
+
+export { Index };
