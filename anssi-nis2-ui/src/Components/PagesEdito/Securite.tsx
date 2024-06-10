351f781a85c8320c0ab813a16cda998415f8747d
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-06-10 12:04:41 +0200

    Ajoute la page "Sécurité"

---
+++ anssi-nis2-ui/src/Components/PagesEdito/Securite.tsx
@@ -0,0 +1,45 @@
+import { DefaultComponent } from "../../Services/Props";
+import encartDesktop from "../../assets/encart-homologation/encartHomologation.desktop.png";
+import encartTablette from "../../assets/encart-homologation/encartHomologation.tablette.png";
+import encartMobile from "../../assets/encart-homologation/encartHomologation.mobile.png";
+
+const Securite: DefaultComponent = () => {
+  return (
+    <>
+      <p>
+        L&apos;ANSSI soumet MonEspaceNIS2 à un rythme d&apos;homologation
+        soutenu (tous les 6 mois) dans le cadre d&apos;une démarche de
+        renforcement continue de la sécurité du service numérique.
+      </p>
+      <p>
+        MonEspaceNIS2 est hébergé chez&nbsp;
+        <a
+          href="https://scalingo.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Scalingo
+        </a>
+        &nbsp;prenant appui sur le Cloud 3DS d&apos;Outscale qualifié
+        SecNumCloud.
+      </p>
+      <img
+        className="encart-homologation encart-desktop"
+        src={encartDesktop}
+        alt="Encart d'homologation de MonEspaceNIS2"
+      />
+      <img
+        className="encart-homologation encart-tablette"
+        src={encartTablette}
+        alt="Encart d'homologation de MonEspaceNIS2"
+      />
+      <img
+        className="encart-homologation encart-mobile"
+        src={encartMobile}
+        alt="Encart d'homologation de MonEspaceNIS2"
+      />
+    </>
+  );
+};
+
+export default Securite;
