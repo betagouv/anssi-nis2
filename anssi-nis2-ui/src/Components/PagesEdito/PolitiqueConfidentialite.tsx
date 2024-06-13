e3958d6ee0f4afe66eef9d705601e1017c66e1f8
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-06-13 17:37:51 +0200

    Ajoute la page de politique de confidentialité

---
+++ anssi-nis2-ui/src/Components/PagesEdito/PolitiqueConfidentialite.tsx
@@ -0,0 +1,458 @@
+import { DefaultComponent } from "../../Services/Props";
+
+const PolitiqueConfidentialite: DefaultComponent = () => {
+  return (
+    <>
+      <p> En vigueur à partir du 12.06.2024</p>
+      <p>
+        Dans le cadre de l&apos;utilisation du portail MonEspaceNIS2 (ci-après
+        le « Site »), l&apos;utilisateur (ci-après l&apos;« Utilisateur ») peut
+        être amené à transmettre à l&apos;Agence nationale de la sécurité des
+        systèmes d&apos;information (ci-après l&apos;« ANSSI ») des données à
+        caractère personnel pour :
+        <ul>
+          <li>
+            inscrire l&apos;Utilisateur et gérer la liste de diffusion de la
+            lettre d&apos;information du service.
+          </li>
+          <li>
+            permettre le bon fonctionnement du service de conversation en ligne
+            accessible depuis la FAQ du site
+          </li>
+        </ul>
+      </p>
+      <p>
+        L&apos;ANSSI, en tant que responsable de traitement, s&apos;engage à ce
+        que la collecte et le traitement des données à caractère personnel de
+        l&apos;Utilisateur soit effectués de manière licite, loyale et
+        transparente, conformément au&nbsp;
+        <a
+          href="https://eur-lex.europa.eu/eli/reg/2016/679/oj?locale=fr"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          Règlement (UE) général sur la protection des données (« RGPD »)
+        </a>
+        &nbsp;et à la&nbsp;
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000886460"
+        >
+          Loi informatique et libertés de 1978 modifiée (« LIL »)
+        </a>
+        . La Politique de confidentialité fait partie intégrante des&nbsp;
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://monservicesecurise.cyber.gouv.fr/cgu"
+        >
+          Conditions générales d&apos;utilisation
+        </a>
+        .
+      </p>
+      <h3>1. Objet</h3>
+      <p>
+        La Politique de confidentialité définit les règles applicables à la
+        collecte et au traitement des données à caractère personnel de
+        l&apos;Utilisateur lors de sa navigation sur le Site et de son
+        utilisation du Site.
+      </p>
+      <p>
+        L&apos;ANSSI est libre de modifier, à tout moment, la Politique de
+        confidentialité, afin notamment de prendre en compte toute évolution
+        législative, réglementaire, jurisprudentielle et technique. La version
+        qui prévaut est&nbsp;
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://monservicesecurise.cyber.gouv.fr/confidentialite"
+        >
+          celle accessible en ligne
+        </a>
+        .
+      </p>
+      <p>
+        L&apos;Utilisateur sera informé en cas de modification de la Politique
+        de confidentialité. S&apos;il s&apos;oppose aux modifications apportées,
+        il est libre de cesser d&apos;utiliser à tout moment les services du
+        Site et de supprimer son compte. Si l&apos;Utilisateur continue à
+        utiliser le Site après la publication ou l&apos;envoi d&apos;un avis
+        concernant les modifications apportées à la Politique de
+        confidentialité, cet usage vaut acceptation de la Politique de
+        confidentialité mise à jour.
+      </p>
+      <h3>2. Identité et coordonnées du responsable de traitement</h3>
+      <p>
+        Le responsable des traitements des données à caractère personnel que
+        l&apos;Utilisateur transmet lors de son inscription sur le Site ou lors
+        de son utilisation est l&apos;ANSSI (51 boulevard de la Tour-Maubourg,
+        75700 Paris 07 SP).
+      </p>
+      <h3>3. Fondement juridique du traitement</h3>
+      <p>
+        Le fondement juridique des traitements se trouve à l&apos;article 6.1.
+        a) du&nbsp;{" "}
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://eur-lex.europa.eu/eli/reg/2016/679/oj?locale=fr"
+        >
+          RGPD
+        </a>
+        , soit le consentement de l&apos;Utilisateur.
+      </p>
+      <p>
+        En effet, l&apos;Utilisateur consent :
+        <ul>
+          <li>
+            pour l&apos;abonnement à la lettre d&apos;information du service, à
+            la collecte de ses données pour l&apos;envoi de la lettre en cochant
+            la case spécifique prévue à cet effet&nbsp;;
+          </li>
+          <li>
+            pour l&apos;utilisation du service de conversation en ligne, à la
+            collecte de ses données d&apos;adresse mail et des données inscrites
+            dans les messages postés.
+          </li>
+        </ul>
+      </p>
+
+      <p>
+        L&apos;utilisateur dispose à tout moment d&apos;un droit de retrait de
+        son consentement, sans porter atteinte à la licéité du traitement fondé
+        sur le consentement effectué avant le retrait de celui-ci.
+      </p>
+      <h3>4. Données à caractère personnel collectées</h3>
+      <h4>A. Données que l&apos;Utilisateur communique à l&apos;ANSSI</h4>
+      <h5>
+        Données collectées pour l&apos;envoi de la lettre d&apos;information du
+        service.
+      </h5>
+      <p>
+        La donnée obligatoire est l&apos;adresse électronique de
+        l&apos;Utilisateur. Une donnée facultative que l&apos;Utilisateur peut
+        fournir est la donnée du nom de l&apos;organisation.
+      </p>
+      <h5>
+        Données collectées dans le cadre du service de conversation en ligne.
+      </h5>
+      <p>
+        Les données collectées sont&nbsp;:
+        <ul>
+          <li> L&apos;adresse mail de l&apos;Utilisateur</li>
+          <li>
+            Les messages envoyés par l&apos;Utilisateur dans le cadre de
+            l&apos;usage du service de conversation en ligne.
+          </li>
+        </ul>
+      </p>
+      <p>
+        Les champs obligatoires sont indiqués lors de la fourniture des données.
+      </p>
+      <p>
+        Cette collecte d&apos;informations se limite au nécessaire, conformément
+        au principe de minimisation des données. Les définitions fournies à
+        l&apos;article 4 du&nbsp;{" "}
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://eur-lex.europa.eu/eli/reg/2016/679/oj?locale=fr"
+        >
+          RGPD
+        </a>
+        &nbsp;sont applicables aux présentes. En cas de mise à jour,
+        l&apos;ANSSI n&apos;abaissera pas le niveau de confidentialité de
+        manière substantielle sans l&apos;information préalable de
+        l&apos;Utilisateur.
+      </p>
+      <h4>B. Journaux de connexion</h4>
+      <p>
+        Des données d&apos;utilisation (adresses IP, ville, pays) historisées
+        sont collectées automatiquement sur le Site lors de la navigation de
+        l&apos;Utilisateur.
+      </p>
+      <p>
+        Celles-ci seront conservées pour une durée ne pouvant être supérieure à
+        13 mois à compter de leur enregistrement et ne sont visibles que des
+        agents chargés de l&apos;administration du Site.
+      </p>
+      <h4>C. Cookie</h4>
+      <h5>Cookie utilisé lors de la navigation sur le Site.</h5>
+      <p>
+        Le Site utilise un petit fichier texte (un « cookie » ou « traceur »)
+        sur le terminal de l&apos;Utilisateur (ordinateur, tablette ou
+        smartphone). Ce cookie est nécessaire à la bonne administration du site.
+        L&apos;utilisateur y consent par défaut mais il est possible de le
+        désactiver au sein de la page{" "}
+        <a href="/gestion-des-cookies">Gestion des cookies</a>.
+      </p>
+      <h3>5. Finalités du traitement</h3>
+      <p>
+        Les données à caractère personnel collectées auprès de
+        l&apos;Utilisateur ont pour finalité :
+        <ul>
+          <li>
+            de pouvoir répondre à l&apos;Utilisateur qui utilise le service de
+            conversation en ligne&nbsp;;
+          </li>
+          <li>
+            la collecte de données statistiques de fréquentation et
+            d&apos;usage.
+          </li>
+        </ul>
+      </p>
+      <p>
+        Si l&apos;Utilisateur souhaite s&apos;abonner à la lettre
+        d&apos;information du service, son adresse électronique collectée a pour
+        finalité :
+        <ul>
+          <li>
+            L&apos;inscription à la lettre d&apos;information du service ;
+          </li>
+          <li>
+            La gestion de la liste de diffusion de cette lettre
+            d&apos;information.
+          </li>
+        </ul>
+      </p>
+      <p>
+        Les logs et le cookie permettent de :
+        <ul>
+          <li>améliorer et faciliter la navigation sur le Site ;</li>
+          <li>
+            diagnostiquer les problèmes du serveur et améliorer la gestion du
+            Site ;
+          </li>
+          <li>
+            résoudre tout litige éventuel ou régler tout problème dans le cadre
+            de l&apos;utilisation du Site&nbsp;;
+          </li>
+          <li>
+            prévenir et lutter contre la fraude informatique (<i>spamming</i>,{" "}
+            <i>hacking…</i>)&nbsp;;
+          </li>
+          <li>
+            élaborer des données statistiques de fréquentation et d&apos;usage.
+          </li>
+        </ul>
+      </p>
+
+      <h3>
+        6. Durée de conservation des données à caractère personnel que
+        l&apos;Utilisateur a communiquées
+      </h3>
+
+      <h5>
+        Données liées à l&apos;inscription à la lettre d&apos;information :
+      </h5>
+      <p>
+        L&apos;adresse électronique de contact pour la lettre d&apos;information
+        est conservée tant que l&apos;Utilisateur est abonné et jusqu&apos;à 6
+        mois après la demande de suppression de ses données telles que prévue au
+        paragraphe précédent ou la demande de suppression de son abonnement.
+      </p>
+      <p>
+        Dans tous les cas, l&apos;ANSSI s&apos;engage à effacer les données à
+        caractère personnel de l&apos;Utilisateur des bases de données du Site à
+        l&apos;issue des durées mentionnées.
+      </p>
+      <p>
+        Toutefois, l&apos;ANSSI peut conserver certaines informations pour une
+        période supérieure à la période de conservation évoquée afin de
+        satisfaire à des obligations légales. Les données de l&apos;Utilisateur
+        seront néanmoins désactivées et ne seront plus accessibles en ligne.
+      </p>
+      <h5>
+        Données liées à l&apos;utilisation du service de conversation en ligne :
+      </h5>
+      <p>
+        Les données renseignées dans le cadre de l&apos;utilisation du service
+        de conversation en ligne sont conservées maximum 1 an à compter de leur
+        collecte.
+      </p>
+      <h3>7. Destinataires des données à caractère personnel</h3>
+      <p>
+        Les destinataires des données à caractère personnel de
+        l&apos;Utilisateur sont le personnel de l&apos;ANSSI impliqué dans
+        l&apos;administration et la gestion du Site ou de la lettre
+        d&apos;information.
+      </p>
+      <h3>8. Sous-traitants et transfert des données à caractère personnel</h3>
+      <p>
+        Des données à caractère personnel sont hébergées en Europe sur les
+        outils suivants&nbsp;et administrés par l&apos;ANSSI :
+        <ul>
+          <li>
+            Pour les données collectées pour la lettre d&apos;information, sur
+            l&apos;outil Brevo
+            <ul>
+              <li>Adresse&nbsp;: 6 boulevard Haussmann, 75008 Paris, France</li>
+              <li>
+                {" "}
+                <a
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  href="https://help.brevo.com/hc/fr/articles/360001005510-Lieux-de-stockage-des-donn%C3%A9es"
+                >
+                  Lieux de stockage
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            Pour les données collectées par le service de conversation en ligne,
+            sur l&apos;outil Crisp
+            <ul>
+              <li>Adresse : 2 Boulevard de Launay, 44100 Nantes, France</li>
+              <li>
+                <a
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  href="https://storage.crisp.chat/public/documents/Crisp%20Privacy%20Policy%20FR.pdf"
+                >
+                  Lieux de stockage
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </p>
+      <h3>9. Sécurité des données à caractère personnel</h3>
+      <p>
+        L&apos;ANSSI met en œuvre toutes les mesures techniques et
+        organisationnelles afin d&apos;assurer la sécurité et la conformité des
+        traitements de données à caractère personnel, ainsi que la
+        confidentialité de ces données.
+      </p>
+      <p>
+        À ce titre, l&apos;ANSSI prend toutes les précautions utiles sur le Site
+        au regard de la nature des données et des risques présentés par le Site,
+        afin de préserver la sécurité des données et, notamment, d&apos;empêcher
+        qu&apos;elles soient déformées, endommagées, ou que des tiers non
+        autorisées y aient accès.
+      </p>
+      <h3>10. Droits de l&apos;Utilisateur</h3>
+      <p>
+        Conformément à la réglementation européenne en vigueur,
+        l&apos;Utilisateur dispose des droits suivants :
+        <ul>
+          <li>
+            droit d&apos;accès (article 15 du&nbsp;{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://eur-lex.europa.eu/eli/reg/2016/679/oj?locale=fr"
+            >
+              RGPD
+            </a>
+            ), de rectification, de mise à jour et de complétude de ses données
+            (article 16 du&nbsp;{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://eur-lex.europa.eu/eli/reg/2016/679/oj?locale=fr"
+            >
+              RGPD
+            </a>
+            )&nbsp;;
+          </li>
+          <li>
+            droit d&apos;effacement de ses données dans les conditions prévues à
+            l&apos;article 17 du&nbsp;{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://eur-lex.europa.eu/eli/reg/2016/679/oj?locale=fr"
+            >
+              RGPD
+            </a>
+            ). L&apos;exercice du droit d&apos;effacement des données par
+            l&apos;Utilisateur entraînera la suppression de son compte et des
+            éventuelles homologations déjà effectuées.
+          </li>
+          <li>
+            droit à la limitation du traitement de ses données (article 18
+            du&nbsp;{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://eur-lex.europa.eu/eli/reg/2016/679/oj?locale=fr"
+            >
+              RGPD
+            </a>
+            )&nbsp;;
+          </li>
+          <li>
+            droit à la portabilité des données qu&apos;il a fournies (article 20
+            du&nbsp;{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://eur-lex.europa.eu/eli/reg/2016/679/oj?locale=fr"
+            >
+              RGPD
+            </a>
+            )
+          </li>
+        </ul>
+      </p>
+      <p>
+        L&apos;Utilisateur peut exercer ses droits en contactant l&apos;ANSSI
+        <ul>
+          <li>
+            par courriel à l&apos;adresse suivante :
+            <a href="mailto:lab-inno@ssi.gouv.fr">lab-inno@ssi.gouv.fr</a>
+          </li>
+          <li>
+            par courriel au délégué à la protection des données des services du
+            Premier ministre à l&apos;adresse suivante&nbsp;:{" "}
+            <a href="mailto:dpd@pm.gouv.fr">dpd@pm.gouv.fr</a>
+          </li>
+          <li>
+            par courrier postal à l&apos;adresse suivante :
+            <p>
+              Agence nationale de la sécurité des systèmes d&apos;information
+              <br />
+              À l&apos;attention du Lab Inno
+              <br />
+              51 boulevard de la Tour-Maubourg
+              <br />
+              75007 PARIS 07 SP
+            </p>
+          </li>
+        </ul>
+      </p>
+      <p>
+        Dans ce cas l&apos;Utilisateur doit préciser quelles sont les données
+        concernées par sa demande et justifier de son identité (fournir une
+        copie d&apos;une pièce d&apos;identité). Par ailleurs, dès que
+        l&apos;ANSSI a connaissance du décès d&apos;un Utilisateur, elle
+        s&apos;engage à détruire ses données, sauf si leur conservation
+        s&apos;avère nécessaire afin de répondre à une obligation légale.
+      </p>
+      <p>
+        Pour la lettre d&apos;information du service, un lien de désinscription
+        est proposé lors de chaque envoi de la lettre.
+      </p>
+      <p>
+        Si l&apos;Utilisateur estime après avoir contacté l&apos;ANSSI que les
+        droits sur ses données n&apos;ont pas été respectés, il peut introduire
+        une réclamation auprès de la CNIL.
+      </p>
+      <p>
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://www.cnil.fr/fr/les-droits-pour-maitriser-vos-donnees-personnelles"
+        >
+          Voir le site de la CNIL pour plus d&apos;informations sur les droits
+          de l&apos;Utilisateur
+        </a>
+        .
+      </p>
+    </>
+  );
+};
+
+export default PolitiqueConfidentialite;
