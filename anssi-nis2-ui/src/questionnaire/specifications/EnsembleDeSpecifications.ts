83fb40a01f51cf33ccd79a0d51d4c99bec4c96d8
Christophe Cadilhac <christophe@malea.dev>
2024-06-18 13:54:59 +0200

    Retourne un `ResultatEligibilite` depuis `EnsembleDeSpecifications`
    On pousse encore d'un cran l'encapsulation.
    `EnsembleDeSpecification` est maintenant en charge de trouver la spécification correspondant aux réponses et d'évaluer son résultat.
    Ça va permettre de gérer plus facilement le cas où plusieurs spécifications correspondent.

--- anssi-nis2-ui/src/questionnaire/specifications/EnsembleDeSpecifications.ts
+++ anssi-nis2-ui/src/questionnaire/specifications/EnsembleDeSpecifications.ts
@@ -1,11 +1,23 @@
 import { Specifications } from "./Specifications.ts";
 import { EtatQuestionnaire } from "../reducerQuestionnaire.ts";
+import { ResultatEligibilite } from "../../../../commun/core/src/Domain/Simulateur/Regulation.definitions.ts";
 
 export class EnsembleDeSpecifications {
   constructor(private readonly specifications: Specifications[]) {}
 
-  premierPassant(reponses: EtatQuestionnaire): Specifications | undefined {
-    return this.specifications.find((s) => s.evalue(reponses) !== undefined);
+  premierPassant(reponses: EtatQuestionnaire): ResultatEligibilite {
+    const premierPassant = this.specifications.find(
+      (s) => s.evalue(reponses) !== undefined,
+    );
+
+    if (!premierPassant) {
+      const detail = JSON.stringify(reponses);
+      throw new Error(
+        `Aucune spécification ne correspond au questionnaire. ${detail}`,
+      );
+    }
+
+    return premierPassant.resultat();
   }
 
   nombre() {
