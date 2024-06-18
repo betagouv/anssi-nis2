8cd7cf4f2651d2a4263acb9c34d27fb94bab46a2
Christophe Cadilhac <christophe@malea.dev>
2024-06-18 10:31:17 +0200

    Branche un test sur le CSV de PROD
    … pour garantir que celui-ci soit fonctionnel.
    Inutile de maintenir une copie du CSV  pour les tests.

--- anssi-nis2-ui/test/questionnaire/specifications/aidesAuxTests.ts
+++ anssi-nis2-ui/test/questionnaire/specifications/aidesAuxTests.ts
@@ -4,3 +4,8 @@ export const leCSV = (nom: string) => {
   const chemin = "./test/questionnaire/specifications/csv/" + nom;
   return readFileSync(chemin).toString("utf-8");
 };
+
+export const leCSVDeProd = (nom: string) => {
+  const chemin = "./src/" + nom;
+  return readFileSync(chemin).toString("utf-8");
+};
