bbbbb37308f105f37bcbfb72dcdfda4bd48159b1
Christophe Cadilhac <christophe@malea.dev>
2024-06-18 14:13:15 +0200

    Sauvegarde les spécifications retenues
    … qui ont permis d'aboutir au résultat d'éligibilité.

--- anssi-nis2-ui/test/questionnaire/specifications/aidesAuxTests.ts
+++ anssi-nis2-ui/test/questionnaire/specifications/aidesAuxTests.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from "node:fs";
+import { ResultatEligibilite } from "../../../../commun/core/src/Domain/Simulateur/Regulation.definitions";
 
 export const leCSV = (nom: string) => {
   const chemin = "./test/questionnaire/specifications/csv/" + nom;
@@ -9,3 +10,11 @@ export const leCSVDeProd = (nom: string) => {
   const chemin = "./src/" + nom;
   return readFileSync(chemin).toString("utf-8");
 };
+
+export function reguleEE(): ResultatEligibilite {
+  return {
+    regulation: "Regule",
+    typeEntite: "EntiteEssentielle",
+    pointsAttention: { resumes: [], precisions: [] },
+  };
+}
