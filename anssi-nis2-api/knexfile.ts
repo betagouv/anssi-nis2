563a91a6aa9ae6bf74a82fa6de053a4206fec11c
Christophe Cadilhac <christophe@malea.dev>
2024-09-23 16:21:41 +0200

    Autorise à fermer les « stale connections »
    La valeur 2 est une valeur legacy. Knex conseille, dans sa doc, d'utiliser 0.
    https://knexjs.org/guide/#pool

--- anssi-nis2-api/knexfile.ts
+++ anssi-nis2-api/knexfile.ts
@@ -5,6 +5,6 @@ config({ path: ".env" });
 module.exports = {
   client: "pg",
   connection: process.env.URL_SERVEUR_BASE_DONNEES,
-  pool: { min: 2, max: 10 },
+  pool: { min: 0, max: 10 },
   migrations: { tableName: "knex_migrations" },
 };
