ce35ff7fc4c94c2db19252ef5a2d275336a52cc1
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2024-09-24 16:25:12 +0200

    Ajoute la sauvegarde des informations de contact
    ... dans la version Express

--- anssi-nis2-api/src/adaptateurs/adaptateurPersistance.ts
+++ anssi-nis2-api/src/adaptateurs/adaptateurPersistance.ts
@@ -1,7 +1,12 @@
 import { DonneesFormulaireSimulateur } from "~core/src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.definitions";
+import { CreeInformationsEmailDto } from "../informations-emails/dto/cree-informations-email.dto";
 
 export interface AdaptateurPersistance {
   sauvegardeReponseFormulaire: (
     donnees: DonneesFormulaireSimulateur,
+  ) => Promise<void>;
+
+  sauvegardeInformationsEmail: (
+    donnees: CreeInformationsEmailDto,
   ) => Promise<void>;
 }
