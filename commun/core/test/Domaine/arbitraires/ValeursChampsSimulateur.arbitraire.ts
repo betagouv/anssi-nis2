b1db751f80bbfddebab39ab509a88f6dd102b3ad
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-10 17:52:00 +0200

    [v1] Supprime le code qui servait uniquement à l'algo `v1`
    … car cet algo a été remplacé partout par la `v2` (le CSV).

--- commun/core/test/Domaine/arbitraires/ValeursChampsSimulateur.arbitraire.ts
+++ commun/core/test/Domaine/arbitraires/ValeursChampsSimulateur.arbitraire.ts
@@ -3,19 +3,12 @@ import {
   AppartenancePaysUnionEuropeenne,
   DesignationOperateurServicesEssentiels,
   TypeEntitePublique,
-  TypeStructure,
-  UnionPetitMoyenGrand,
-  ValeurChampSimulateur,
 } from "../../../src/Domain/Simulateur/ChampsSimulateur.definitions";
 import {
-  ValeursappartenancePaysUnionEuropeenne,
   ValeursPetitMoyenGrand,
   ValeursTypeEntitePublique,
 } from "../../../src/Domain/Simulateur/ChampsSimulateur.valeurs";
-import { fabriqueArbSingleton } from "../../utilitaires/manipulationArbitraires.fabriques";
 
-export const arbDesignationOperateurServicesEssentiels_ToujoursOui =
-  fc.constant<DesignationOperateurServicesEssentiels>("oui");
 export const arbDesignationOperateurServicesEssentiels_ToujoursNon =
   fc.constant<DesignationOperateurServicesEssentiels>("non");
 export const arbDesignationOperateurServicesEssentiels_ToujoursNeSaitPas =
@@ -46,33 +39,3 @@ export const arbTypeStructure_Publique = fc.constant("publique" as const);
 export const arbTypeEntitePublique = fc.constantFrom<TypeEntitePublique>(
   ...ValeursTypeEntitePublique
 );
-export const arbDesigneOperateurServicesEssentiels: ArbitraireChampFormulaire<DesignationOperateurServicesEssentiels> =
-  {
-    oui: fc.constant<DesignationOperateurServicesEssentiels[]>(["oui"]),
-    non: fc.constant<DesignationOperateurServicesEssentiels[]>(["non"]),
-    nsp: fc.constant<DesignationOperateurServicesEssentiels[]>(["nsp"]),
-  };
-export const arbTypeStructure: ArbitraireChampFormulaire<TypeStructure> = {
-  privee: fc.constant<TypeStructure[]>(["privee"]),
-  publique: fc.constant<TypeStructure[]>(["publique"]),
-};
-export const arbTranche: ArbitraireChampFormulaire<UnionPetitMoyenGrand> = {
-  petit: fc.constant<UnionPetitMoyenGrand[]>(["petit"]),
-  moyen: fc.constant<UnionPetitMoyenGrand[]>(["moyen"]),
-  grand: fc.constant<UnionPetitMoyenGrand[]>(["grand"]),
-};
-export const arbappartenancePaysUnionEuropeenne: ArbitraireChampFormulaire<
-  AppartenancePaysUnionEuropeenne,
-  "franceOuAutre" | "tout" | "horsFrance"
-> = {
-  france: fc.constant(["france"]),
-  horsue: fc.constant(["horsue"]),
-  autre: fc.constant(["autre"]),
-  franceOuAutre: fabriqueArbSingleton(["france", "autre"]),
-  horsFrance: fabriqueArbSingleton(["horsue", "autre"]),
-  tout: fabriqueArbSingleton(ValeursappartenancePaysUnionEuropeenne),
-};
-export type ArbitraireChampFormulaire<
-  T extends ValeurChampSimulateur,
-  TypesAdditionnelles extends string = T
-> = Record<T | TypesAdditionnelles, fc.Arbitrary<T[]>>;
