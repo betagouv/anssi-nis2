67d0c35f4ec53f21f2938af622a7d95fa7e1bba8
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-10 14:56:17 +0200

    Ajoute `RestezInformes` sur la page de résultat V2
    Ça manquait par rapport à la V1.

--- anssi-nis2-ui/src/Components/Simulateur/EtapeResultatV2.tsx
+++ anssi-nis2-ui/src/Components/Simulateur/EtapeResultatV2.tsx
@@ -3,17 +3,28 @@ import { ResultatEligibilite } from "../../../../commun/core/src/Domain/Simulate
 import { PointsAttention } from "./Resultats/PointsAttention.tsx";
 import { RowContainer } from "../General/RowContainer.tsx";
 import { CenteredContainer } from "../General/CenteredContainer.tsx";
+import { getModeFormulaireEmail } from "./SimulateurEtapeResult.aide.ts";
+import RestezInformes from "../RestezInformes.tsx";
 
 export function EtapeResultatV2(props: { resultat: ResultatEligibilite }) {
   return (
-    <RowContainer>
-      <CenteredContainer>
-        <TamponResultat resultat={props.resultat} />
-        <PointsAttention
-          resumes={props.resultat.pointsAttention.resumes}
-          precisions={props.resultat.pointsAttention.precisions}
-        />
-      </CenteredContainer>
-    </RowContainer>
+    <>
+      <RowContainer>
+        <CenteredContainer>
+          <TamponResultat resultat={props.resultat} />
+          <PointsAttention
+            resumes={props.resultat.pointsAttention.resumes}
+            precisions={props.resultat.pointsAttention.precisions}
+          />
+        </CenteredContainer>
+      </RowContainer>
+      <RowContainer className="fr-background-alt--blue-france">
+        <CenteredContainer>
+          <RestezInformes
+            mode={getModeFormulaireEmail(props.resultat.regulation)}
+          />
+        </CenteredContainer>
+      </RowContainer>
+    </>
   );
 }
