2b299c8ae6b28a52e65f6ccbebc4822e06659bf0
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-11 16:25:38 +0200

    [v1] Supprime le code qui servait uniquement à l'algo `v1`
    … car cet algo a été remplacé partout par la `v2` (le CSV).

--- commun/utils/services/sets.operations.ts
+++ commun/utils/services/sets.operations.ts
@@ -1,10 +1,8 @@
-export const ensembleNeutre = new Set();
 export const ensembleNeutreDe = <T>() => new Set<T>();
 export const union = <T>(set1: Set<T>, set2: Set<T>) =>
   new Set([...set1, ...set2]);
 
 export const ens = <T>(...elements: T[]) => new Set(elements);
-export const fromArray = <T>(elements: T[]) => ens(...elements);
 
 export const tous =
   <T>(predicat: (element: T) => boolean) =>
