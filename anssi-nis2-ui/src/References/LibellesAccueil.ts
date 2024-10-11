653ba3b14ff222ae00852fc20cb9750c30beb88e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-11 11:45:46 +0200

    [v1] Supprime le code qui servait uniquement à l'algo `v1`
    … car cet algo a été remplacé partout par la `v2` (le CSV).

--- anssi-nis2-ui/src/References/LibellesAccueil.ts
+++
@@ -1,6 +0,0 @@
-export const libellesAccueil = {
-  concerne: {
-    titre: "Mon entité est-elle concernée ?",
-    resume: "Déterminez si votre entité est régulée par la directive NIS 2",
-  },
-};
