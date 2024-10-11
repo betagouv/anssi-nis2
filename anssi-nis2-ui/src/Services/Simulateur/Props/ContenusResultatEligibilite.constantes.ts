51c815b4a4fa2299a84caf2478e06c05e2cd2745
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-11 11:53:51 +0200

    [v1] Supprime le code qui servait uniquement à l'algo `v1`
    … car cet algo a été remplacé partout par la `v2` (le CSV).

--- anssi-nis2-ui/src/Services/Simulateur/Props/ContenusResultatEligibilite.constantes.ts
+++
@@ -1,6 +0,0 @@
-import { PrecisionsResultatProps } from "./ContenusResultatEligibilite.declaration.ts";
-
-export const precisionsResultatVide: PrecisionsResultatProps = {
-  principal: "",
-  annexe: "",
-};
