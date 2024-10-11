6f4e63833f8d2d109813a426429c2e414d97db85
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-11 12:23:16 +0200

    [v1] Supprime le code qui servait uniquement à l'algo `v1`
    … car cet algo a été remplacé partout par la `v2` (le CSV).

--- commun/core/test/Domaine/DonneesFormulaire.predicats.spec.ts
+++ commun/core/test/Domaine/DonneesFormulaire.predicats.spec.ts
@@ -1,15 +1,14 @@
-import { fc } from "@fast-check/vitest";
 import { describe, expect, it } from "vitest";
-import { UnionPetitMoyenGrand } from "../../src/Domain/Simulateur/ChampsSimulateur.definitions";
-import { donneesFormulaireSimulateurVide } from "../../src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.constantes";
-import { fabriqueDonneesFormulaire } from "../../src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.fabrique";
+import {
+  donneesFormulaireSimulateurVide,
+} from "../../src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.constantes";
+import {
+  fabriqueDonneesFormulaire,
+} from "../../src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.fabrique";
 import { auMoinsUneActiviteListee } from "../../src/Domain/Simulateur/Activite.predicats";
 import {
-  contientPetiteEntreprise,
   predicatDonneesFormulaire,
 } from "../../src/Domain/Simulateur/services/DonneesFormulaire/DonneesFormulaire.predicats";
-
-import { fabriqueArbSingleton } from "../utilitaires/manipulationArbitraires.fabriques";
 
 describe("predicatDonneesFormulaire", () => {
   describe("Champ contient", () => {
@@ -41,49 +40,4 @@ describe("predicatDonneesFormulaire", () => {
     });
   });
 
-  describe(contientPetiteEntreprise, () => {
-    it("est vrai pour petit / petit", () => {
-      expect(
-        contientPetiteEntreprise(
-          fabriqueDonneesFormulaire({
-            trancheNombreEmployes: ["petit"],
-            trancheChiffreAffaire: ["petit"],
-          }),
-        ),
-      ).toBeTruthy();
-    });
-
-    const moyenGrand: UnionPetitMoyenGrand[] = ["moyen", "grand"];
-    const petitMoyenGrand: UnionPetitMoyenGrand[] = ["petit", "moyen", "grand"];
-    const verifieDonneesNeContientPasPetiteEntreprise = (
-      trancheA: UnionPetitMoyenGrand[],
-      trancheB: UnionPetitMoyenGrand[],
-    ) =>
-      !contientPetiteEntreprise(
-        fabriqueDonneesFormulaire({
-          trancheNombreEmployes: trancheA,
-          trancheChiffreAffaire: trancheB,
-        }),
-      );
-    it("est faux pour toute tranche employes moyen et grand", () => {
-      fc.assert(
-        fc.property(
-          fabriqueArbSingleton(moyenGrand),
-          fabriqueArbSingleton(petitMoyenGrand),
-          verifieDonneesNeContientPasPetiteEntreprise,
-        ),
-        { verbose: true },
-      );
-    });
-    it("est faux pour toute tranche CA moyen et grand", () => {
-      fc.assert(
-        fc.property(
-          fabriqueArbSingleton(petitMoyenGrand),
-          fabriqueArbSingleton(moyenGrand),
-          verifieDonneesNeContientPasPetiteEntreprise,
-        ),
-        { verbose: true },
-      );
-    });
-  });
 });
