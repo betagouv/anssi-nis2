4966be21af8376e15fedc0e6f0f272f566d16efe
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-14 14:19:18 +0200

    [v1] Supprime le code qui servait uniquement à l'algo `v1`
    … car cet algo a été remplacé partout par la `v2` (le CSV).

--- commun/core/src/Domain/Simulateur/Regulation.constantes.ts
+++
@@ -1,9 +0,0 @@
-import { CausesRegulation } from "./Regulation.definitions";
-
-export const causeReguleOSE: CausesRegulation = {
-  DesignationOperateurServicesEssentiels: {
-    designationOperateurServicesEssentiels: "oui",
-  },
-};
-
-
