392846fb87d5afcfe452c5de5347a77e883e8a13
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2024-10-14 14:52:22 +0200

    [Nest] Supprime la possibilité d'instancier une implémentation Nest
    L'implémentation Express est la seule qui est désormais utilisée.
    Les commits suivants vont supprimer tout le code lié à Nest.

--- anssi-nis2-api/src/main.ts
+++ anssi-nis2-api/src/main.ts
@@ -1,5 +1,5 @@
 import { creeServeur } from "./serveur";
-import { DependanceServeur, ImplementationDuServeur } from "./serveur.types";
+import { DependanceServeur } from "./serveur.types";
 import { AdaptateurPersistancePostgres } from "./adaptateurs/adaptateurPersistance.postgres";
 import { AdaptateurJournalPostgres } from "./adaptateurs/adaptateurJournal.postgres";
 import { AdaptateurCrmBrevo } from "./adaptateurs/adaptateurCrm.brevo";
@@ -8,37 +8,29 @@ import { AdaptateurGestionErreurMemoire } from "./adaptateurs/adaptateurGestionE
 import { AdaptateurProtectionRateLimit } from "./adaptateurs/adaptateurProtection.rateLimit";
 import { AdaptateurProtectionMemoire } from "./adaptateurs/adaptateurProtection.memoire";
 
-const { Nest, Express } = ImplementationDuServeur;
-
 const portEcoute = Number(process.env.PORT) || 3000;
 
-const implementation =
-  process.env.AVEC_IMPLEMENTATION_EXPRESSJS === "true" ? Express : Nest;
+const dependances: DependanceServeur = {
+  adaptateurPersistance: new AdaptateurPersistancePostgres(),
+  adaptateurJournal: new AdaptateurJournalPostgres(),
+  adaptateurCrm: new AdaptateurCrmBrevo(
+    new URL(process.env.BREVO_API_BASE_URL),
+    process.env.BREVO_CLE_API,
+  ),
+  adaptateurGestionErreur: process.env.SENTRY_DSN
+    ? new AdaptateurGestionErreurSentry(
+        process.env.SENTRY_DSN,
+        process.env.SENTRY_ENVIRONNEMENT,
+        process.env.SENTRY_CHEMINS_IGNORES_PAR_TRACING?.split(",") ?? [],
+        Number(process.env.SENTRY_SAMPLE_RATE_DU_TRACING) || 0,
+      )
+    : new AdaptateurGestionErreurMemoire(),
+  adaptateurProtection: process.env.LIMITATION_REQUETES_COURTE_DUREE
+    ? new AdaptateurProtectionRateLimit()
+    : new AdaptateurProtectionMemoire(),
+};
 
-const dependances: DependanceServeur =
-  process.env.AVEC_IMPLEMENTATION_EXPRESSJS === "true"
-    ? {
-        adaptateurPersistance: new AdaptateurPersistancePostgres(),
-        adaptateurJournal: new AdaptateurJournalPostgres(),
-        adaptateurCrm: new AdaptateurCrmBrevo(
-          new URL(process.env.BREVO_API_BASE_URL),
-          process.env.BREVO_CLE_API,
-        ),
-        adaptateurGestionErreur: process.env.SENTRY_DSN
-          ? new AdaptateurGestionErreurSentry(
-              process.env.SENTRY_DSN,
-              process.env.SENTRY_ENVIRONNEMENT,
-              process.env.SENTRY_CHEMINS_IGNORES_PAR_TRACING?.split(",") ?? [],
-              Number(process.env.SENTRY_SAMPLE_RATE_DU_TRACING) || 0,
-            )
-          : new AdaptateurGestionErreurMemoire(),
-        adaptateurProtection: process.env.LIMITATION_REQUETES_COURTE_DUREE
-          ? new AdaptateurProtectionRateLimit()
-          : new AdaptateurProtectionMemoire(),
-      }
-    : null;
-
-creeServeur(portEcoute, implementation, dependances).then((serveur) =>
+creeServeur(portEcoute, dependances).then((serveur) =>
   serveur.ecoute(() =>
     console.log(`Serveur MonEspaceNIS2 écoute sur le port ${portEcoute}`),
   ),
--- anssi-nis2-api/src/serveur.ts
+++ anssi-nis2-api/src/serveur.ts
@@ -1,17 +1,9 @@
-import { creeServeurNest } from "./serveur.nest";
 import { creeServeurExpress } from "./serveur.express";
-import {
-  DependanceServeur,
-  ImplementationDuServeur,
-  ServeurMonEspaceNIS2,
-} from "./serveur.types";
+import { DependanceServeur, ServeurMonEspaceNIS2 } from "./serveur.types";
 
 export async function creeServeur(
   port: number,
-  implementation: ImplementationDuServeur,
   dependances?: DependanceServeur,
 ): Promise<ServeurMonEspaceNIS2> {
-  return implementation === ImplementationDuServeur.Nest
-    ? creeServeurNest(port)
-    : creeServeurExpress(port, dependances);
+  return creeServeurExpress(port, dependances);
 }
