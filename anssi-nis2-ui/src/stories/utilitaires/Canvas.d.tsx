08687e7b477358cfdbf5cb058bf92e053aa4de2d
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-02-17 15:45:52 +0100

    [Storybook] Supprime les tests Storybook
    Ces tests font, depuis peu, échouer la CI.
    Une erreur liée à playwright…
    La valeur de ces tests est trop faible pour s'embêter à passer du temps à les maintenir. On les supprime.

--- anssi-nis2-ui/src/stories/utilitaires/Canvas.d.tsx
+++
@@ -1,4 +0,0 @@
-import { BoundFunctions } from "@testing-library/dom/types/get-queries-for-element";
-import * as queries from "@testing-library/dom/types/queries";
-
-export type CanvasObject = BoundFunctions<typeof queries>;
