4e288b8ea6f66655e5a86c6dabbb51baa712a53b
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-03-18 11:42:59 +0100

    [Accueil] Utilise un logo pour symboliser « NIS 2 »

--- anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauAccueil.tsx
@@ -2,10 +2,13 @@ import { DefaultComponent } from "../../Services/Props";
 import BlocPrincipal from "../BlocPrincipal.tsx";
 import Marches from "../../assets/marches.svg";
 import MarchesTablette from "../../assets/marches-tablette.svg";
+import Logo from "../../assets/logo-nis2.svg";
 
 const BandeauAccueil: DefaultComponent = () => (
   <BlocPrincipal className="fond-primaire" id="hero">
-    <h1 className="texte-blanc">Directive NIS&nbsp;2</h1>
+    <h1 className="texte-blanc">
+      Directive <img src={Logo} alt="NIS 2" />
+    </h1>
     <h2 className="texte-blanc">
       Élever collectivement
       <br /> notre niveau de cybersécurité
