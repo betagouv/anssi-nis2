b084b630e92acfd35eda4097589d12b158166d4a
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-03-27 17:13:35 +0100

    Ajoute une entrée de menu vers MesServicesCyber
    … seulement lorsque le feature flag lié à MSC est actif.

--- anssi-nis2-ui/src/Components/MenuDesktop.tsx
+++ anssi-nis2-ui/src/Components/MenuDesktop.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { DefaultComponent } from "../Services/Props";
+import { bandeauMscPasEncoreVisible } from "./bandeauMscPasEncoreVisible.tsx";
 
 export const MenuDesktop: DefaultComponent = () => {
   return (
@@ -7,6 +8,15 @@ export const MenuDesktop: DefaultComponent = () => {
       <NavLink to="/">Accueil</NavLink>
       <NavLink to="/directive">M&apos;informer sur la directive</NavLink>
       <NavLink to="/simulateur">Tester si mon entité est concernée</NavLink>
+      {bandeauMscPasEncoreVisible() ? null : (
+        <NavLink
+          to="https://messervices.cyber.gouv.fr/nis2/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Me faire accompagner
+        </NavLink>
+      )}
       <NavLink to="/infolettre">M&apos;abonner à la newsletter</NavLink>
       <NavLink
         to="https://aide.monespacenis2.cyber.gouv.fr"
