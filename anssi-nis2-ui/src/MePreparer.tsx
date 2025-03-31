2bc977cdb877947d38509184ed580f90a6c78853
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-03-31 17:05:25 +0200

    Ajoute la section « Déterminer … » à la page « Me préparer »

--- anssi-nis2-ui/src/MePreparer.tsx
+++ anssi-nis2-ui/src/MePreparer.tsx
@@ -34,6 +34,11 @@ const MePreparer: DefaultComponent = () => {
                   titre: "S'informer sur la directive NIS 2",
                 },
                 { id: "se-tenir-informes", titre: "Se tenir informés" },
+                {
+                  id: "determiner-si-concernee-par-nis2",
+                  titre:
+                    "Déterminer si votre entité est concernée par la directive NIS 2",
+                },
               ]}
             />
             <div className="contenu-page">
@@ -119,6 +124,73 @@ const MePreparer: DefaultComponent = () => {
                     S&apos;inscrire à l&apos;infolettre
                   </a>
                 </p>
+              </BlocPrincipal>
+
+              <BlocPrincipal
+                id="determiner-si-concernee-par-nis2"
+                className="fond-blanc fr-pt-0"
+              >
+                <h2 className="texte-primaire">
+                  Déterminer si votre entité est concernée par la directive
+                  NIS&nbsp;2
+                </h2>
+
+                <p>
+                  Le simulateur mis à disposition par l&apos;ANSSI propose un
+                  parcours simple et rapide permettant d&apos;avoir une première
+                  indication sur le potentiel statut essentiel ou important de
+                  votre entité. Nous vous conseillons donc, dans le cas où le
+                  statut de votre entité vis-à-vis de la directive NIS&nbsp;2 ne
+                  serait pas encore déterminé, de réaliser le parcours proposé
+                  par le simulateur.
+                </p>
+                <p>
+                  Quelques conseils pour une meilleure utilisation de ce
+                  simulateur :
+                  <ul>
+                    <li>
+                      Il est préférable d&apos;associer des compétences
+                      juridiques à la démarche d&apos;auto-évaluation du statut
+                      de l&apos;entité afin de déterminer si celle-ci entre dans
+                      le périmètre des entités régulées par la directive
+                      NIS&nbsp;2.
+                    </li>
+                    <li>
+                      Il a été observé que certaines entités, lors de leur
+                      utilisation du simulateur, confondent le secteur de
+                      réalisation de leurs propres activités avec le secteur
+                      d&apos;activité des clients qu&apos;elles fournissent ou
+                      dont elles sont les sous-traitants. Une telle confusion
+                      fausse les résultats du test. Les entités utilisant le
+                      simulateur sont donc invitées à renseigner les différentes
+                      rubriques avec le plus grand soin.
+                    </li>
+                    <ul>
+                      <li>
+                        À titre d&apos;exemple, un fournisseur de turbine
+                        d&apos;éolienne, pouvant se considérer comme étant du
+                        secteur «&nbsp;Énergie&nbsp;», sera en réalité associé
+                        au secteur «&nbsp;Fabrication&nbsp;» (correspondant à
+                        l&apos;industrie manufacturière) selon le prisme établi
+                        par la directive NIS&nbsp;2.
+                      </li>
+                      <li>
+                        Les entités du secteur de l&apos;industrie
+                        manufacturière sont invitées à consulter avec attention
+                        le secteur «&nbsp;Fabrication&nbsp;» de ce simulateur,
+                        qui regroupe plus de 500 activités distinctes.
+                      </li>
+                    </ul>
+                  </ul>
+                  En tout état de cause, recourir au simulateur, dont le
+                  résultat est dépendant de l&apos;exactitude des données
+                  fournies en entrée, ne dispense pas d&apos;une analyse au vu
+                  des textes en vigueur et des circonstances propres à chaque
+                  entité.
+                </p>
+                <a className="fr-nis2-bouton-principal" href="/simulateur">
+                  Accéder au simulateur
+                </a>
               </BlocPrincipal>
             </div>
           </div>
