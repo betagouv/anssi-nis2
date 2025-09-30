7bf0640b984e481715d707d2e08822f6f3ee57bb
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-09-30 11:02:27 +0200

    Utilise <lab-anssi-presentation-anssi>
    … afin d'avoir une présentation à jour de l'Agence.
    C'est la v1.28 qui embarque la présentation à jour. Dans cette version, on n'a plus de `jsx.d.ts` c'est pour cela que ce commit en embarque une.

--- anssi-nis2-ui/src/Components/Accueil/BandeauQuiEstANSSI.tsx
+++ anssi-nis2-ui/src/Components/Accueil/BandeauQuiEstANSSI.tsx
@@ -1,28 +1,8 @@
-import AnssiLogo from "../../assets/ANSSI-180.svg";
-import { liens } from "../../References/liens.tsx";
 import BlocPrincipal from "../BlocPrincipal.tsx";
 
 const BandeauQuiEstANSSI = () => (
-  <BlocPrincipal id="anssi">
-    <h2 className="texte-primaire">Qu&apos;est-ce que l&apos;ANSSI ?</h2>
-    <div className="conteneur-logo">
-      <img src={AnssiLogo} alt="Logo ANSSI" />
-    </div>
-    <p>
-      Créée en 2009, l&apos;Agence nationale de la sécurité des systèmes
-      d’information (ANSSI) est l’autorité nationale en matière de cybersécurité
-      et de cyberdéfense.
-    </p>
-    <p>
-      <b>
-        Son action pour la protection de la Nation face aux cyberattaques se
-        traduit en quatre grandes missions : défendre, connaître, partager,
-        accompagner.
-      </b>
-    </p>
-    <a {...liens.anssi.site} className="fr-nis2-bouton-secondaire">
-      En savoir plus
-    </a>
+  <BlocPrincipal>
+    <lab-anssi-presentation-anssi></lab-anssi-presentation-anssi>
   </BlocPrincipal>
 );
 
---
+++ anssi-nis2-ui/src/lab-anssi-ui-kit.jsx.d.ts
@@ -0,0 +1,21 @@
+// Fichier créé manuellement, car depuis l'UI Kit v1.26, il n'est plus généré automatiquement.
+// En théorie, l'UI kit devrait le fournir, mais comme solution de contournement, on le crée manuellement.
+declare namespace JSX {
+  interface IntrinsicElements {
+    "lab-anssi-centre-aide": {
+      nomService: string;
+      liens: string;
+    };
+    "lab-anssi-bouton-suite-cyber-navigation": {
+      sourceUtm?: string;
+    };
+    "lab-anssi-navigation-pied-de-page": {
+      conforme?: boolean;
+    };
+    "lab-anssi-mes-services-cyber-bandeau": object;
+    "lab-anssi-resume-pssi": {
+      nomService: string;
+    };
+    "lab-anssi-presentation-anssi": object;
+  }
+}
