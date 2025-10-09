1edd3d2dd83f1ce0ea6de4a4291d4f562132956c
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-10-09 14:31:17 +0200

    [npm] Supprime des addons storybooks inutiles
    … qui ne nous apportent pas de réelle valeur.
    On est sur un train de commits visant à supprimer les dépendances inutiles.
    Un passage à Storybook 9 est également envisagé, pour permettre d'autres mises à jour qui fermeront des alertes dependabot.

--- anssi-nis2-ui/src/stories/Components/Transverses/RestezInformes.stories.tsx
+++ anssi-nis2-ui/src/stories/Components/Transverses/RestezInformes.stories.tsx
@@ -1,17 +1,9 @@
 import { Meta, StoryObj } from "@storybook/react";
 import RestezInformes from "../../../Components/RestezInformes.tsx";
-import { genereDecorateurPourContexte } from "../../utilitaires/generateursDecorateurs.tsx";
-import { defaultContext } from "../../utilitaires/PageDecorator.tsx";
-import { Contexte } from "../../../Services/contexte";
-
-const enregistreEmailContexte: Contexte = {
-  ...defaultContext,
-};
 
 const meta: Meta<typeof RestezInformes> = {
   title: "Composants/Transverses/Restez informés",
   component: RestezInformes,
-  decorators: [genereDecorateurPourContexte(enregistreEmailContexte)],
   parameters: {
     actions: {
       handles: ["enregistreInformationsEmail"],
