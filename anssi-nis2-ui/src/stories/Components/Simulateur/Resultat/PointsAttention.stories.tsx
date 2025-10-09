3792dad77e99d36ebfb748ab3ef1924611acec64
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-10-09 14:36:47 +0200

    [Storybook] Upgrade vers Storybook v9
    … en utilisant
    `npx storybook@latest upgrade`
    … qui vient de https://storybook.js.org/docs/releases/migration-guide-from-older-version
    
    Le wizard de mise à jour indique que tout s'est bien passé.
    Et `npm run storybook` exécute Storybook avec succès.

--- anssi-nis2-ui/src/stories/Components/Simulateur/Resultat/PointsAttention.stories.tsx
+++ anssi-nis2-ui/src/stories/Components/Simulateur/Resultat/PointsAttention.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta } from "@storybook/react";
+import { Meta } from "@storybook/react-vite";
 import { PointsAttention } from "../../../../Components/Simulateur/Resultats/PointsAttention.tsx";
 import {
   CodesPrecisionsPointsAttention,
--- anssi-nis2-ui/src/stories/Components/Transverses/RestezInformes.stories.tsx
+++ anssi-nis2-ui/src/stories/Components/Transverses/RestezInformes.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react-vite";
 import RestezInformes from "../../../Components/RestezInformes.tsx";
 
 const meta: Meta<typeof RestezInformes> = {
